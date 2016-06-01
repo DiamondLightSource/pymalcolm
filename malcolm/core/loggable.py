3a4b392769000af96720514c39908dbd728a97f5
Tom Cobb <tom.cobb@diamond.ac.uk>
2016-06-01 11:17:31 +0100

    Added Loggable and tests

---
+++ malcolm/core/loggable.py
@@ -0,0 +1,20 @@
+import logging
+
+class Loggable(object):
+    """Utility class that provides a named logger for a class instance"""
+
+    def __init__(self, logger_name):
+        super(Loggable, self).__init__()
+        self.set_logger_name(logger_name)
+
+    def set_logger_name(self, logger_name):
+        """Change the name of the logger that log_* should call
+
+        Args:
+            logger_name (str): Name of the logger
+        """
+        self._logger_name = logger_name
+        self._logger = logging.getLogger(self._logger_name)
+        # set self.log_debug = self._logger.debug etc.
+        for n in "debug warning info error exception".split():
+            setattr(self, "log_%s" % n, getattr(self._logger, n))
---
+++ tests/test_core/test_loggable.py
@@ -0,0 +1,29 @@
+#!/bin/env dls-python
+import unittest
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+# mock
+from pkg_resources import require
+require("mock")
+from mock import patch
+
+# module imports
+from malcolm.core.loggable import Loggable
+
+class TestLoggable(unittest.TestCase):
+
+    @patch("malcolm.core.loggable.logging")
+    def test_init(self, mock_logging):
+        l = Loggable("foo")
+        mock_logging.getLogger.assert_called_once_with("foo")
+
+    @patch("malcolm.core.loggable.logging")
+    def test_debug_calls_logger_function(self, mock_logging):
+        l = Loggable("bar")
+        l.log_debug("hello")
+        l._logger.debug.assert_called_once_with("hello")
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
