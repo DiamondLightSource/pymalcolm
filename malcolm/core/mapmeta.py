194ac08e6da0d1f2b8b25a47430dc3f4d530c768
Gary Yendell <gary.yendell@diamond.ac.uk>
2016-06-01 15:28:25 +0100

    Make docstring correction

--- malcolm/core/mapmeta.py
+++ malcolm/core/mapmeta.py
@@ -21,7 +21,13 @@ class MapMeta(Loggable):
         Args:
             attribute_meta(AttributeMeta): Attribute instance to store
             required(bool): Whether attribute is required or optional
+
+        Raises:
+            ValueError:
         """
 
-        self.elements[attribute_meta.name] = attribute_meta
-        self.required.append(required)
+        if attribute_meta.name in self.elements.keys:
+            raise ValueError("Element already exists in map")
+        else:
+            self.elements[attribute_meta.name] = attribute_meta
+            self.required.append(required)
