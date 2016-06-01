d7343ac93dbfb62b3f425711b8df9929a1d6f1ad
Gary Yendell <gary.yendell@diamond.ac.uk>
2016-06-01 14:48:01 +0100

    Remove uneccesary dict definition, clarify doc string

--- malcolm/core/mapmeta.py
+++ malcolm/core/mapmeta.py
@@ -5,13 +5,13 @@ from malcolm.core.attributemeta import AttributeMeta
 
 
 class MapMeta(Loggable):
-    """A meta object to store a set of attribute metas"""
+    """An object containing a set of AttributeMeta objects"""
 
     def __init__(self, name):
         super(MapMeta, self).__init__(logger_name=name)
 
         self.name = name
-        self.elements = OrderedDict({})
+        self.elements = OrderedDict()
 
     def add_element(self, attribute_meta, required=False):
         """
