997a6eefa31e8fe0dc1f3b7df743eb52e2cc501b
Gary Yendell <gary.yendell@diamond.ac.uk>
2016-06-03 16:41:13 +0100

    Create wscomms package, Create WSServerComms and MalcomlmWebSocketHandler classes

---
+++ tests/test_wsservercomms.py
@@ -0,0 +1,92 @@
+import unittest
+from collections import OrderedDict
+
+from pkg_resources import require
+require("mock")
+require('tornado')
+from mock import MagicMock, patch
+
+from malcolm.wscomms.wsservercomms import WSServerComms
+from malcolm.wscomms.wsservercomms import MalcolmWebSocketHandler
+
+
+class TestWSServerComms(unittest.TestCase):
+
+    def setUp(self):
+        self.p = MagicMock()
+
+    @patch('malcolm.wscomms.wsservercomms.Application')
+    @patch('malcolm.wscomms.wsservercomms.IOLoop')
+    def test_init(self, _, _1):
+        self.WS = WSServerComms("TestWebSocket", self.p, 1)
+
+        self.assertEqual("TestWebSocket", self.WS.name)
+        self.assertEqual(self.p, self.WS.process)
+
+    @patch('malcolm.wscomms.wsservercomms.Application.listen')
+    @patch('malcolm.wscomms.wsservercomms.IOLoop')
+    def test_setup(self, ioloop_mock, listen_mock):
+        loop_mock = MagicMock()
+        ioloop_mock.current.return_value = loop_mock
+
+        self.WS = WSServerComms("TestWebSocket", self.p, 1)
+
+        listen_mock.assert_called_once_with(1)
+        ioloop_mock.current.assert_called_once_with()
+        self.assertEqual(self.WS.process,
+                         self.WS.WSApp.handlers[0][1][0].handler_class.process)
+
+    @patch('malcolm.wscomms.wsservercomms.Application.listen')
+    @patch('malcolm.wscomms.wsservercomms.IOLoop')
+    def test_start(self, ioloop_mock, _):
+        loop_mock = MagicMock()
+        ioloop_mock.current.return_value = loop_mock
+        self.WS = WSServerComms("TestWebSocket", self.p, 1)
+        self.WS.start_recv_loop()
+
+        loop_mock.start.assert_called_once_with()
+
+    @patch('malcolm.wscomms.wsservercomms.Application.listen')
+    @patch('malcolm.wscomms.wsservercomms.IOLoop')
+    def test_stop(self, ioloop_mock, _):
+        loop_mock = MagicMock()
+        ioloop_mock.current.return_value = loop_mock
+        self.WS = WSServerComms("TestWebSocket", self.p, 1)
+        self.WS.stop_recv_loop()
+
+        loop_mock.stop.assert_called_once_with()
+
+    @patch('malcolm.wscomms.wsservercomms.Request')
+    @patch('malcolm.wscomms.wsservercomms.json')
+    @patch('malcolm.wscomms.wsservercomms.Application.listen')
+    @patch('malcolm.wscomms.wsservercomms.IOLoop')
+    def test_MWSH_on_message(self, _, _1, json_mock, request_mock):
+        self.WS = WSServerComms("TestWebSocket", self.p, 1)
+
+        message_dict = dict(name="TestMessage")
+        json_mock.loads.return_value = message_dict
+
+        request = MagicMock()
+        request.context = self.WS.WSApp.handlers[0][1][0].handler_class
+        request_mock.from_dict.return_value = request
+
+        m = MagicMock()
+        MWSH = MalcolmWebSocketHandler(m, m)
+        self.WS.WSApp.handlers[0][1][0].handler_class.on_message(
+            MWSH, "TestMessage")
+
+        json_mock.loads.assert_called_once_with("TestMessage",
+                                                object_pairs_hook=OrderedDict())
+        request_mock.from_dict.assert_called_once_with(message_dict)
+        self.p.handle_request.assert_called_once_with(request)
+
+    @patch('malcolm.wscomms.wsservercomms.Application.listen')
+    @patch('malcolm.wscomms.wsservercomms.IOLoop')
+    def test_startup(self, _, _2):
+        self.WS = WSServerComms("TestWebSocket", self.p, 1)
+
+        response_mock = MagicMock()
+        self.WS.send_to_client(response_mock)
+
+
+
