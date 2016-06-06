409e59e49f013641d7630c9e0939736dc3b0e01f
Tom Cobb <tom.cobb@diamond.ac.uk>
2016-06-06 11:55:22 +0100

    Moved HelloController into malcolm.controllers

--- tests/test_core/test_system_core.py
+++ tests/test_core/test_system_core.py
@@ -8,39 +8,11 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 # logging.basicConfig(level=logging.DEBUG)
 
 # module imports
-from malcolm.core.controller import Controller
-from malcolm.core.method import Method
-from malcolm.core.mapmeta import MapMeta
-from malcolm.core.stringmeta import StringMeta
+from malcolm.controllers.hellocontroller import HelloController
 from malcolm.core.block import Block
 from malcolm.core.process import Process
 from malcolm.core.syncfactory import SyncFactory
 from malcolm.core.request import Request
-
-
-class HelloController(Controller):
-    def say_hello(self, args):
-        """Says Hello to name
-
-        Args:
-            name (str): The name of the person to say hello to
-
-        Returns:
-            str: The greeting
-        """
-        return dict(greeting="Hello %s" % args["name"])
-
-    def create_methods(self):
-        """Create a Method wrapper for say_hello and return it"""
-        method = Method("say_hello")
-        method.set_function(self.say_hello)
-        takes = MapMeta("takes")
-        takes.add_element(StringMeta("name"))
-        method.set_function_takes(takes)
-        returns = MapMeta("returns")
-        returns.add_element(StringMeta("greeting"))
-        method.set_function_returns(returns)
-        yield method
 
 
 class TestSystemCore(unittest.TestCase):
