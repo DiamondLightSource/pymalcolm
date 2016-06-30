a17df1c0b471211ab1c0871dfd9d25ca62e9d6d7
Charles Mita <charles.mita@diamond.ac.uk>
2016-06-30 11:36:50 +0100

    Rename bm variable to meta in test_meta

--- tests/test_core/test_meta.py
+++ tests/test_core/test_meta.py
@@ -12,72 +12,72 @@ from malcolm.core.serializable import Serializable
 
 class TestMeta(unittest.TestCase):
     def test_init(self):
-        bm = Meta("meta_name", "meta_description")
-        self.assertEqual("meta_name", bm.name)
-        self.assertEqual("meta_description", bm.description)
-        self.assertEqual([], bm.tags)
-        self.assertEqual("malcolm:core/Meta:1.0", bm.typeid)
+        meta = Meta("meta_name", "meta_description")
+        self.assertEqual("meta_name", meta.name)
+        self.assertEqual("meta_description", meta.description)
+        self.assertEqual([], meta.tags)
+        self.assertEqual("malcolm:core/Meta:1.0", meta.typeid)
 
     def test_set_description(self):
-        bm = Meta("meta_name", "meta_description")
-        bm.on_changed = Mock(wrap=bm.on_changed)
-        bm.set_description("new_description")
-        self.assertEquals("new_description", bm.description)
-        bm.on_changed.assert_called_once_with(
+        meta = Meta("meta_name", "meta_description")
+        meta.on_changed = Mock(wrap=meta.on_changed)
+        meta.set_description("new_description")
+        self.assertEquals("new_description", meta.description)
+        meta.on_changed.assert_called_once_with(
             [["description"], "new_description"], True)
 
     def test_set_tags(self):
-        bm = Meta("meta_name", "meta_description")
-        bm.on_changed = Mock(wrap=bm.on_changed)
-        bm.set_tags(["new_tag"])
-        self.assertEquals(["new_tag"], bm.tags)
-        bm.on_changed.assert_called_once_with(
+        meta = Meta("meta_name", "meta_description")
+        meta.on_changed = Mock(wrap=meta.on_changed)
+        meta.set_tags(["new_tag"])
+        self.assertEquals(["new_tag"], meta.tags)
+        meta.on_changed.assert_called_once_with(
             [["tags"], ["new_tag"]], True)
 
     def test_update_description(self):
-        bm = Meta("meta_name", "")
-        bm.on_changed = Mock(wrap=bm.on_changed)
-        bm.update([["description"], "new_description"])
-        self.assertEquals(bm.description, "new_description")
-        bm.on_changed.assert_called_once_with(
+        meta = Meta("meta_name", "")
+        meta.on_changed = Mock(wrap=meta.on_changed)
+        meta.update([["description"], "new_description"])
+        self.assertEquals(meta.description, "new_description")
+        meta.on_changed.assert_called_once_with(
             [["description"], "new_description"], True)
 
     def test_update_tags(self):
-        bm = Meta("meta_name", "")
-        bm.on_changed = Mock(wrap=bm.on_changed)
-        bm.update([["tags"], ["new_tag"]])
-        self.assertEquals(bm.tags, ["new_tag"])
-        bm.on_changed.assert_called_once_with(
+        meta = Meta("meta_name", "")
+        meta.on_changed = Mock(wrap=meta.on_changed)
+        meta.update([["tags"], ["new_tag"]])
+        self.assertEquals(meta.tags, ["new_tag"])
+        meta.on_changed.assert_called_once_with(
             [["tags"], ["new_tag"]], True)
 
     def test_invalid_update_raises(self):
-        bm = Meta("meta_name", "")
-        self.assertRaises(ValueError, bm.update, [["invalid_path"], ""])
+        meta = Meta("meta_name", "")
+        self.assertRaises(ValueError, meta.update, [["invalid_path"], ""])
 
     def test_substructure_update_raises(self):
-        bm = Meta("meta_name", "")
+        meta = Meta("meta_name", "")
         self.assertRaises(
-            ValueError, bm.update, [["tags", "invalid_substructure_path"], ""])
+            ValueError, meta.update, [["tags", "invalid_substructure_path"], ""])
 
     def test_to_dict(self):
-        bm = Meta("meta_name", "meta_description")
-        bm.tags = ["tag"]
+        meta = Meta("meta_name", "meta_description")
+        meta.tags = ["tag"]
         expected = OrderedDict()
         expected["description"] = "meta_description"
         expected["tags"] = ["tag"]
         expected["typeid"] = "malcolm:core/Meta:1.0"
-        self.assertEquals(expected, bm.to_dict())
+        self.assertEquals(expected, meta.to_dict())
 
     def test_from_dict(self):
         d = {"description":"test_description", "tags":["tag1", "tag2"],
              "typeid":"malcolm:core/Meta:1.0"}
-        bm = Serializable.from_dict("meta_name", d)
-        self.assertEqual(Meta, type(bm))
-        self.assertEqual("meta_name", bm.name)
-        self.assertEqual("test_description", bm.description)
-        self.assertEqual(["tag1", "tag2"], bm.tags)
-        self.assertEqual("malcolm:core/Meta:1.0", bm.typeid)
-        self.assertEqual(d, bm.to_dict())
+        meta = Serializable.from_dict("meta_name", d)
+        self.assertEqual(Meta, type(meta))
+        self.assertEqual("meta_name", meta.name)
+        self.assertEqual("test_description", meta.description)
+        self.assertEqual(["tag1", "tag2"], meta.tags)
+        self.assertEqual("malcolm:core/Meta:1.0", meta.typeid)
+        self.assertEqual(d, meta.to_dict())
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
