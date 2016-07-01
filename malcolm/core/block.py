50accc9e469afc6f4b6668bdb0cec8d28c9ae81d
Tom Cobb <tom.cobb@diamond.ac.uk>
2016-07-01 15:18:47 +0100

    Refactor Block
    * _methods -> methods
    * _attributes -> attributes
    * DummyLock is now a class to simplify understanding
    * add_method and add_attributes now call add_child
    * added replace_children method

--- malcolm/core/block.py
+++ malcolm/core/block.py
@@ -1,5 +1,4 @@
 from collections import OrderedDict
-from contextlib import contextmanager
 
 from malcolm.core.serializable import Serializable
 from malcolm.core.request import Request
@@ -7,19 +6,30 @@ from malcolm.core.response import Response
 from malcolm.core.attribute import Attribute
 from malcolm.core.method import Method
 
-@contextmanager
-def dummy_lock():
-    yield
+class DummyLock(object):
+
+    def acquire(self):
+        pass
+
+    def release(self):
+        pass
+
+    def __enter__(self):
+        self.acquire()
+
+    def __exit__(self, type, value, traceback):
+        self.release()
+        return False
 
 class LockRelease(object):
     def __init__(self, lock):
         self.lock = lock
 
     def __enter__(self):
-        self.lock.__exit__(None, None, None)
+        self.lock.release()
 
     def __exit__(self, type, value, traceback):
-        self.lock.__enter__()
+        self.lock.acquire()
         return False
 
 @Serializable.register("malcolm:core/Block:1.0")
@@ -33,34 +43,40 @@ class Block(Serializable):
         """
         super(Block, self).__init__(name=name)
         self.name = name
-        self._methods = OrderedDict()
-        self._attributes = OrderedDict()
-        self.lock = dummy_lock()
+        self.methods = OrderedDict()
+        self.attributes = OrderedDict()
+        self.lock = DummyLock()
 
-    def add_attribute(self, attribute):
+    def add_attribute(self, attribute, notify=True):
         """Add an Attribute to the block and set the block as its parent"""
+        self.add_child(attribute, self.attributes)
+        self.on_changed([[attribute.name], attribute.to_dict()], notify)
 
-        assert attribute.name not in self._attributes, \
-            "Attribute %s already defined for Block %s" \
-            % (attribute.name, self.name)
-        self._attributes[attribute.name] = attribute
-        attribute.set_parent(self)
-        setattr(self, attribute.name, attribute)
-        self.on_changed([[attribute.name], attribute.to_dict()])
-        self.notify_subscribers()
-
-    def add_method(self, method):
+    def add_method(self, method, notify=True):
         """Add a Method to the Block
 
         Args:
             method (Method): The Method object that has already been filled in
         """
-        assert method.name not in self._methods, \
-            "Method %s already defined for Block %s" % (method.name, self.name)
-        self._methods[method.name] = method
-        setattr(self, method.name, method)
-        self.on_changed([[method.name], method.to_dict()])
-        self.notify_subscribers()
+        self.add_child(method, self.methods)
+        self.on_changed([[method.name], method.to_dict()], notify)
+
+    def add_child(self, attribute_or_method, d):
+        """Add an Attribute or Method to the block and set the block as its
+        parent, but don't notify"""
+        child_name = attribute_or_method.name
+        assert not hasattr(self, child_name), \
+            "Attribute or Method %s already defined for Block %s" \
+            % (child_name, self.name)
+        setattr(self, child_name, attribute_or_method)
+        d[child_name] = attribute_or_method
+        attribute_or_method.set_parent(self)
+
+    def _where_child_stored(self, child):
+        if isinstance(child, Method):
+            return self.methods
+        elif isinstance(child, Attribute):
+            return self.attributes
 
     def update(self, change):
         """Update block given a single change.
@@ -78,16 +94,25 @@ class Block(Serializable):
             # sub-structure does not exist - create and add
             if len(change[0]) > 1:
                 raise ValueError("Missing substructure at %s" % name)
-            obj = Serializable.from_dict(name, change[1])
-            if isinstance(obj, Method):
-                self.add_method(obj)
-            elif isinstance(obj, Attribute):
-                self.add_attribute(obj)
-            else:
-                raise ValueError(
-                    "Change %s deserialized to unknown object %s"
-                    % (change, obj))
+            child = Serializable.from_dict(name, change[1])
+            d = self._where_child_stored(child)
+            assert d is not None, \
+                "Change %s deserialized to unknown object %s" % (change, child)
+            self.add_child(child, d)
 
+    def replace_children(self, children, notify=True):
+        for method_name in self.methods:
+            delattr(self, method_name)
+        self.methods.clear()
+        for attr_name in self.attributes:
+            delattr(self, attr_name)
+        self.attributes.clear()
+        for child in children:
+            d = self._where_child_stored(child)
+            assert d is not None, \
+                "Don't know how to add a child %s" % child
+            self.add_child(child, d)
+        self.on_changed([[], self.to_dict()], notify)
 
     def notify_subscribers(self):
         if self.parent is not None:
@@ -106,11 +131,11 @@ class Block(Serializable):
         with self.lock:
             if request.type_ == Request.POST:
                 method_name = request.endpoint[-1]
-                response = self._methods[method_name].get_response(request)
+                response = self.methods[method_name].get_response(request)
             elif request.type_ == Request.PUT:
                 attr_name = request.endpoint[-1]
-                self._attributes[attr_name].put(request.value)
-                self._attributes[attr_name].set_value(request.value)
+                self.attributes[attr_name].put(request.value)
+                self.attributes[attr_name].set_value(request.value)
                 response = Response.Return(request.id_, request.context)
             self.parent.block_respond(response, request.response_queue)
 
@@ -120,9 +145,9 @@ class Block(Serializable):
         d = OrderedDict()
 
         d["typeid"] = self.typeid
-        for attribute_name, attribute in self._attributes.items():
+        for attribute_name, attribute in self.attributes.items():
             d[attribute_name] = attribute.to_dict()
-        for method_name, method in self._methods.items():
+        for method_name, method in self.methods.items():
             d[method_name] = method.to_dict()
 
 
