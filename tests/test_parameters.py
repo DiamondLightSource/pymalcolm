1edd2c157534d71cd54c0e45f8755df4fcb51570
Tom Cobb <tom.cobb@diamond.ac.uk>
2016-07-29 10:39:48 +0100

    Move takes to parameters

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -3,34 +3,34 @@ import setup_malcolm_paths
 import unittest
 from mock import Mock, patch, call, MagicMock
 
-from malcolm import takes
+from malcolm import parameters
 from malcolm.core.method import REQUIRED
 from malcolm.metas import NumberMeta, StringMeta
 
 
-class TestTakes(unittest.TestCase):
+class TestParameters(unittest.TestCase):
 
     def test_imports(self):
         decorated = {}
-        for k in dir(takes):
-            v = getattr(takes, k)
+        for k in dir(parameters):
+            v = getattr(parameters, k)
             if hasattr(v, "Method"):
                 decorated[k] = v
         self.assertEqual(decorated, dict(
-            string=takes.string,
-            float64=takes.float64,
-            int32=takes.int32))
+            string=parameters.string,
+            float64=parameters.float64,
+            int32=parameters.int32))
 
     def test_make_string_meta(self):
         params = Mock()
         params.name = "me"
         params.description = "desc"
         del params.default
-        self.assertEqual(list(takes.string.Method.takes.elements),
+        self.assertEqual(list(parameters.string.Method.takes.elements),
                          ["name", "description", "default"])
-        default_meta = takes.string.Method.takes.elements["default"]
+        default_meta = parameters.string.Method.takes.elements["default"]
         self.assertIsInstance(default_meta, StringMeta)
-        meta, default = takes.string(params)
+        meta, default = parameters.string(params)
         self.assertEqual(default, REQUIRED)
         self.assertEqual(meta.name, "me")
         self.assertEqual(meta.description, "desc")
@@ -41,10 +41,10 @@ class TestTakes(unittest.TestCase):
         params.name = "me"
         params.description = "desc"
         params.default = 32
-        default_meta = takes.int32.Method.takes.elements["default"]
+        default_meta = parameters.int32.Method.takes.elements["default"]
         self.assertIsInstance(default_meta, NumberMeta)
         self.assertEqual(default_meta.dtype, "int32")
-        meta, default = takes.int32(params)
+        meta, default = parameters.int32(params)
         self.assertEqual(default, 32)
         self.assertEqual(meta.name, "me")
         self.assertEqual(meta.description, "desc")
@@ -56,7 +56,7 @@ class TestTakes(unittest.TestCase):
         params.name = "me"
         params.description = "desc"
         params.default = 32.6
-        meta, default = takes.float64(params)
+        meta, default = parameters.float64(params)
         self.assertEqual(default, 32.6)
         self.assertEqual(meta.name, "me")
         self.assertEqual(meta.description, "desc")
