1973b1be2b06b1ce5491ff104ced4d25ba0deedf
Tom Cobb <tom.cobb@diamond.ac.uk>
2016-08-10 14:34:03 +0100

    Revert overzealous import reduction

--- malcolm/core/vmetas/__init__.py
+++ malcolm/core/vmetas/__init__.py
@@ -1,6 +1,10 @@
-# make the import path nice
-from malcolm.packageutil import import_methodmeta_decorated_classes
-
-__all__ = import_methodmeta_decorated_classes(globals(), __name__)
-
-del import_methodmeta_decorated_classes
+from malcolm.core.vmetas.tablemeta import TableMeta
+from malcolm.core.vmetas.stringmeta import StringMeta
+from malcolm.core.vmetas.stringarraymeta import StringArrayMeta
+from malcolm.core.vmetas.pointgeneratormeta import PointGeneratorMeta
+from malcolm.core.vmetas.numberarraymeta import NumberArrayMeta
+from malcolm.core.vmetas.numbermeta import NumberMeta
+from malcolm.core.vmetas.choicearraymeta import ChoiceArrayMeta
+from malcolm.core.vmetas.choicemeta import ChoiceMeta
+from malcolm.core.vmetas.booleanmeta import BooleanMeta
+from malcolm.core.vmetas.booleanarraymeta import BooleanArrayMeta
