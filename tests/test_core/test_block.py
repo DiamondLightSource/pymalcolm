82817797b48699638468df78ffa2781670838773
Tom Cobb <tom.cobb@diamond.ac.uk>
2016-08-12 12:25:07 +0100

    Added block test

--- tests/test_core/test_block.py
+++ tests/test_core/test_block.py
@@ -9,7 +9,8 @@ import unittest
 from mock import MagicMock, call, patch
 
 # module imports
-from malcolm.core.block import Block
+from malcolm.core import Block, Attribute
+from malcolm.core.vmetas import NumberMeta
 
 
 class TestBlock(unittest.TestCase):
@@ -18,6 +19,18 @@ class TestBlock(unittest.TestCase):
         b = Block()
         self.assertEqual(list(b), [])
         self.assertEqual("malcolm:core/Block:1.0", b.typeid)
+
+    def test_getattr(self):
+        b = Block()
+        a = Attribute(NumberMeta("int32"))
+        b.replace_endpoints(dict(a=a))
+        def f(meta, value):
+            a.set_value(value)
+        b.set_writeable_functions(dict(a=f))
+        b.a = 32
+        self.assertEqual(b.a, 32)
+
+
 
 
 if __name__ == "__main__":
