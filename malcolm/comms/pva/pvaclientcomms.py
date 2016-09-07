cdd10634876aea6ab6dd615c5b11f438f085bb07
Alan Greer <alan.greer@diamond.ac.uk>
2016-09-07 11:56:09 +0100

    Implemented Get and Post message handling within the pvaclientcomms class.

--- malcolm/comms/pva/pvaclientcomms.py
+++ malcolm/comms/pva/pvaclientcomms.py
@@ -1,12 +1,13 @@
 from collections import OrderedDict
 
+from malcolm.comms.pva.pvautil import PvaUtil
 from malcolm.core import ClientComms, Request, Subscribe, Response, \
     deserialize_object, serialize_object
-from malcolm.core.request import Get, Return, Error
+from malcolm.core.request import Get, Post, Return, Error
 import pvaccess
 
 
-class PvaClientComms(ClientComms):
+class PvaClientComms(ClientComms, PvaUtil):
     """A class for a client to communicate with the server"""
 
     def __init__(self, process, _=None):
@@ -28,25 +29,57 @@ class PvaClientComms(ClientComms):
         self.log_debug("Request: %s", request)
 
         if isinstance(request, Get):
-            self.log_debug("Endpoint: %s", request["endpoint"][0])
+            self.log_debug("Get called with endpoint: %s", request["endpoint"])
             try:
                 # Connect to the channel
                 c = pvaccess.Channel(request["endpoint"][0])
+                # Create the path request from the endpoints (not including the block name endpoint)
+                path = ""
+                for item in request["endpoint"][1:]:
+                    path = path + item + "."
+                self.log_debug("path: %s", path[:-1])
                 # Perform a get and record the response
-                response = c.get(request["endpoint"][1])
+                response = c.get(path[:-1])
                 self.log_debug("Response: %s", response)
                 # Now create the Return object and populate it with the response
-                return_object = Return(id_=request["id"], value=response.toDict())
+                value=response.toDict(True)
+                if 'typeid' in value:
+                    if value['typeid'] == 'malcolm:core/Error:1.0':
+                        return_object = Error(id_=request["id"], message=value['message'])
+                    else:
+                        return_object = Return(id_=request["id"], value=value)
             except:
                 # PvAccess error, create the Error message
-                return_object = Error(id_=request["id"], message="To be determined")
+                return_object = Error(id_=request["id"], message="PvAccess error")
 
             self.log_debug("Return object: %s", return_object)
             self.send_to_caller(return_object)
 
+        elif isinstance(request, Post):
+            self.log_debug("Endpoint: %s", request["endpoint"][0])
+            self.log_debug("Parameters: %s", request["parameters"])
+            try:
+                method = pvaccess.PvObject({'method': pvaccess.STRING})
+                method.set({'method': request["endpoint"][1]})
+                # Connect to the channel and create the RPC client
+                rpc = pvaccess.RpcClient(request["endpoint"][0], method)
+                # Construct the pv object from the parameters
+                params = self.dict_to_pv_object(request["parameters"])
+                self.log_debug("PvObject parameters: %s", params)
+                # Call the method on the RPC object
+                response = rpc.invoke(params)
+                self.log_debug("Response: %s", response)
+                # Now create the Return object and populate it with the response
+                value=response.toDict(True)
+                if 'typeid' in value:
+                    if value['typeid'] == 'malcolm:core/Error:1.0':
+                        return_object = Error(id_=request["id"], message=value['message'])
+                    else:
+                        return_object = Return(id_=request["id"], value=value)
+            except:
+                # PvAccess error, create the Error message
+                return_object = Error(id_=request["id"], message="PvAccess error")
 
-#    def subscribe_server_blocks(self, _):
-#        """Subscribe to process blocks"""
-#        request = Subscribe(None, None, [".", "blocks", "value"])
-#        request.set_id(self.SERVER_BLOCKS_ID)
-#        self.loop.add_callback(self.send_to_server, request)
+            self.log_debug("Return object: %s", return_object)
+            self.send_to_caller(return_object)
+
