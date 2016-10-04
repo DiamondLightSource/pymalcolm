a94acb6dbaf11810299310af919df38ecb27cf8a
Tom Cobb <tom.cobb@diamond.ac.uk>
2016-10-04 16:23:36 +0100

    Added AsynOutportPart

---
+++ malcolm/parts/asyn/asynoutportpart.py
@@ -0,0 +1,24 @@
+from malcolm.parts.ca.castringpart import CAStringPart
+from malcolm.controllers.defaultcontroller import DefaultController
+from malcolm.core import method_takes, REQUIRED
+from malcolm.core.vmetas import StringMeta
+
+@method_takes(
+    "name", StringMeta("Name of the created attribute"), REQUIRED,
+    "description", StringMeta("Desc of created attribute"), REQUIRED,
+    "rbv", StringMeta("Full pv of demand and default for rbv"), REQUIRED,
+    "outport_type", StringMeta("Type (like 'CS' or 'NDArray')"), REQUIRED,
+)
+class AsynOutportPart(CAStringPart):
+    def create_tags(self, params):
+        tags = ["widget:textupdate"]
+        return tags
+
+    @DefaultController.Resetting
+    def connect_pvs(self, _=None):
+        super(AsynOutportPart, self).connect_pvs()
+        # Add the outport tags
+        tags = [t for t in self.attr.meta.tags
+                if not t.startswith("flowgraph:outport:")]
+        tags.append("flowgraph:outport:%s:%s" % (
+            self.params.outport_type, self.attr.value))
