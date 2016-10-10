aecbd1fd72e8ac5257ea8f38887986f4b7cba075
Tom Cobb <tom.cobb@diamond.ac.uk>
2016-10-10 15:20:49 +0100

    Added deadtime compensation for detectordriver part

--- malcolm/parts/ADCore/detectordriverpart.py
+++ malcolm/parts/ADCore/detectordriverpart.py
@@ -1,11 +1,21 @@
 from malcolm.core import method_takes, REQUIRED
-from malcolm.core.vmetas import PointGeneratorMeta
+from malcolm.core.vmetas import PointGeneratorMeta, NumberMeta
 from malcolm.parts.builtin.layoutpart import LayoutPart
 from malcolm.controllers.runnablecontroller import RunnableController
 from malcolm.parts.ADCore.hdfwriterpart import DatasetSourceInfo
 
 
 class DetectorDriverPart(LayoutPart):
+    # Attributes
+    readoutTime = None
+
+    def create_attributes(self):
+        for data in super(DetectorDriverPart, self).create_attributes():
+            yield data
+        self.readoutTime = NumberMeta(
+            "float64", "Time taken to readout detector").make_attribute(0.002)
+        yield "readoutTime", self.readoutTime, self.readoutTime.set_value
+
     @RunnableController.PreConfigure
     def report_info(self, _):
         return [DatasetSourceInfo("detector", "primary")]
@@ -14,15 +24,16 @@ class DetectorDriverPart(LayoutPart):
     @method_takes(
         "generator", PointGeneratorMeta("Generator instance"), REQUIRED)
     def configure(self, task, completed_steps, steps_to_do, part_info, params):
-        exposures = set()
+        durations = set()
         for i in range(completed_steps, completed_steps + steps_to_do):
             point = params.generator.get_point(i)
-            exposures.add(point.duration)
-        assert len(exposures) == 1, \
-            "Expected a fixed duration time, got %s" % list(exposures)
-        exposure = exposures.pop()
+            durations.add(point.duration)
+        assert len(durations) == 1, \
+            "Expected a fixed duration time, got %s" % list(durations)
+        exposure = durations.pop()
         assert exposure is not None, \
             "Expected duration to be specified, got None"
+        exposure -= self.readoutTime.value
         task.put({
             self.child["exposure"]: exposure,
             self.child["imageMode"]: "Multiple",
