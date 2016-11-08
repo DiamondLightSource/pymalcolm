5df5889b634730b8c96b0b0e3eafe9e8a8edd019
Tom Cobb <tom.cobb@diamond.ac.uk>
2016-11-08 12:57:40 +0000

    Added PMAC simulator

--- examples/pmac_simulator.py
+++ examples/pmac_simulator.py
@@ -1,49 +1,43 @@
 #!/bin/env dls-python
 import os
 import sys
+import logging
+
+# Start some logging
+#logging.basicConfig(level=logging.DEBUG)
 
 from pkg_resources import require
-require("tornado", "numpy")
+require("numpy", "ruamel.yaml", "scanpointgenerator", "cothread")
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
-from malcolm.core import SyncFactory, Process
-from malcolm.controllers import ManagerController
-from malcolm.comms.websocket.websocketservercomms import WebsocketServerComms
-from malcolm.includes.pandabox.hardware_collection import hardware_collection
-
-# Input params
-HOSTNAME = "localhost"
-PORT = 8888
-WSPORT = 8080
+from malcolm.core import SyncFactory, Process, Task
+from malcolm.blocks.demo import SimulatorPMACManager
+from scanpointgenerator import LineGenerator, CompoundGenerator, \
+    FixedDurationMutator
 
 # Make the top level objects
 sf = SyncFactory("Sync")
 process = Process("Process", sf)
 
-# Add the websocket server
-params = WebsocketServerComms.MethodMeta.prepare_input_map(port=WSPORT)
-comms = WebsocketServerComms(process, params)
-process.add_comms(comms)
+# Make the malcolm object
+params = SimulatorPMACManager.MethodMeta.prepare_input_map(
+    mriPrefix="TST-PMAC")
+SimulatorPMACManager(process, params)
+sim = process.get_block("TST-PMAC")
 
-# We daemonise the server by double forking, but we leave the controlling
-# terminal and other file connections alone.
-if False and os.fork():
-    # Exit first parent
-    sys.exit(0)
-# Do second fork to avoid generating zombies
-if False and os.fork():
-    sys.exit(0)
+# Start the process
+process.start()
 
-# Make the parts by querying the PandABox
-params = hardware_collection.MethodMeta.prepare_input_map(
-    mriPrefix="P", hostname=HOSTNAME, port=PORT)
-_, parts = hardware_collection(process, params)
+# Wait for everything to settle down
+task = Task("waiter", process)
+task.when_matches(sim["state"], "Idle", timeout=10)
 
-# Make a controller for the top level
-params = ManagerController.MethodMeta.prepare_input_map(mri="P")
-controller = ManagerController(process, parts, params)
+# Do a test
+xs = LineGenerator("m1", "mm", -8., -12., 121, alternate_direction=True)
+ys = LineGenerator("m2", "mm", -4., -6., 21)
+gen = CompoundGenerator([ys, xs], [], [FixedDurationMutator(0.005)])
+for i in range(1):
+    sim.configure(gen)
+    sim.run()
 
-# Start the server
-for comms in process.comms:
-    comms.start()
-process.recv_loop()
+#process.stop()
