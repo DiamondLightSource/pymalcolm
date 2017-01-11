b7c94c3253de161f3e67e645d1088f5499001b72
Pete Leicester <peter.leicester@diamond.ac.uk>
2017-01-11 13:46:47 +0000

    Added correction for skew in fine motor scan

--- malcolm/parts/demo/i08scanfinepart.py
+++ malcolm/parts/demo/i08scanfinepart.py
@@ -4,22 +4,24 @@ from malcolm.controllers.runnablecontroller import RunnableController
 
 
 class I08ScanFinePart(RunnableChildPart):
-    def _goto_midpoint(self, task, params, name="X"):
+    def _get_midpoint(self, params, name="X"):
         # Make some very specific assumptions about the generator
 	generators = params.generator.generators
         g = [g for g in generators if g.name[0] == "T1%sF" % name][0]
         mid = float(g.start[0] + g.stop[0]) / 2
-	futures = task.put_async(self.child["positionT1%sC" % name], mid)
-	return futures
+        return mid
 
     # MethodMeta will be filled in by _update_configure_args
     @RunnableController.Configure
     @method_takes()
     def configure(self, task, completed_steps, steps_to_do, part_info, params):
-	fs = self._goto_midpoint(task, params, "X")
-	fs += self._goto_midpoint(task, params, "Y")
+        xp = self._get_midpoint(params, "X")
+        yp = self._get_midpoint(params, "Y")
+        xp += 0.0075 * yp
+        yp += 0.0075 * xp
+	fs = task.put_async(self.child["positionT1XC"], xp)
+	fs += task.put_async(self.child["positionT1YC"], yp)
 	task.wait_all(fs)
-	task.sleep(1)
         super(I08ScanFinePart, self).configure(
             task, completed_steps, steps_to_do, part_info, params)
 
