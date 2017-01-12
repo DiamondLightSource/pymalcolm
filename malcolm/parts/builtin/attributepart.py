50b1f6683abad630afc70b8d581414b9a51a13c5
Tom Cobb <tom.cobb@diamond.ac.uk>
2017-01-12 16:31:10 +0000

    Add velocity settle to motors and refactor common bits

---
+++ malcolm/parts/builtin/attributepart.py
@@ -0,0 +1,46 @@
+from malcolm.core import Part, method_takes, REQUIRED
+from malcolm.core.vmetas import StringMeta, BooleanMeta, ChoiceMeta
+from malcolm.tags import widget_types, widget
+
+
+@method_takes(
+    "name", StringMeta("Name of the created attribute"), REQUIRED,
+    "description", StringMeta("Desc of created attribute"), REQUIRED,
+    "widget", ChoiceMeta("Widget type", [""] + widget_types), "",
+    "writeable", BooleanMeta("Is the attribute writeable?"), False,
+)
+class AttributePart(Part):
+    # Attribute instance
+    attr = None
+
+    def create_attributes(self):
+        # Find the tags
+        tags = self.create_tags()
+        # Make a meta object for our attribute
+        meta = self.create_meta(self.params.description, tags)
+        # The attribute we will be publishing
+        initial_value = self.get_initial_value()
+        self.attr = meta.make_attribute(initial_value)
+        writeable_func = self.get_writeable_func()
+        yield self.params.name, self.attr, writeable_func
+
+    def create_meta(self, description, tags):
+        raise NotImplementedError()
+
+    def get_writeable_func(self):
+        if self.params.writeable:
+            writeable_func = self.attr.set_value
+        else:
+            writeable_func = None
+        return writeable_func
+
+    def create_tags(self):
+        tags = []
+        if self.params.widget:
+            tags.append(widget(self.params.widget))
+        return tags
+
+    def get_initial_value(self):
+        """Implement this to override the attribute's initial value at creation
+        """
+        return None
--- malcolm/parts/builtin/float64part.py
+++ malcolm/parts/builtin/float64part.py
@@ -1,14 +1,14 @@
 from malcolm.parts.builtin.attributepart import AttributePart
 from malcolm.core import method_also_takes
-from malcolm.core.vmetas import StringMeta
+from malcolm.core.vmetas import NumberMeta
 
 
 @method_also_takes(
-    "initialValue", StringMeta("Initial value of attribute"), "",
+    "initialValue", NumberMeta("float64", "Initial value of attribute"), 0.0,
 )
 class StringPart(AttributePart):
     def get_initial_value(self):
         return self.params.initialValue
 
     def create_meta(self, description, tags):
-        return StringMeta(description=description, tags=tags)
+        return NumberMeta("float64", description=description, tags=tags)
