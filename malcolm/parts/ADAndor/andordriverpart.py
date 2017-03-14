1a033cf6203bd4b620d59ceb1a1b3fd9727f65c3
Tom Cobb <tom.cobb@diamond.ac.uk>
2017-03-14 13:50:27 +0000

    Added excalibur

--- malcolm/parts/ADAndor/andordriverpart.py
+++ malcolm/parts/ADAndor/andordriverpart.py
@@ -1,11 +1,9 @@
-from malcolm.core import method_takes, REQUIRED
-from malcolm.core.vmetas import PointGeneratorMeta
-from malcolm.controllers.runnablecontroller import RunnableController
 from malcolm.parts.ADCore.detectordriverpart import DetectorDriverPart
 
 
 class AndorDriverPart(DetectorDriverPart):
     def post_configure(self, task, params):
-        task.put(self.child["acquirePeriod"], self.child.exposure + self.readout_time.value)
+        task.put(self.child["acquirePeriod"],
+                 self.child.exposure + self.readout_time.value)
         super(AndorDriverPart, self).post_configure(task, params)
 
