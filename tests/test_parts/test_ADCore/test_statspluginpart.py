2e2afea0a5d590b44efbe85d474c01a867f23da3
Tom Cobb <tom.cobb@diamond.ac.uk>
2017-03-15 11:44:59 +0000

    Made stats plugin write its own XML attribute file

--- tests/test_parts/test_ADCore/test_statspluginpart.py
+++ tests/test_parts/test_ADCore/test_statspluginpart.py
@@ -21,18 +21,38 @@ class TestStatsPluginPart(unittest.TestCase):
         self.child.__getitem__.side_effect = getitem
 
         self.params = MagicMock()
+        self.params.mri = "BLOCK-STAT"
         self.process.get_block.return_value = self.child
         self.o = StatsPluginPart(self.process, self.params)
+
+    def test_report_info(self):
+        infos = self.o.report_info(ANY)
+        self.assertEqual(len(infos), 1)
+        self.assertEqual(infos[0].name, "sum")
+        self.assertEqual(infos[0].attr, "StatsTotal")
 
     def test_configure(self):
         task = MagicMock()
         completed_steps = ANY
         steps_to_do = ANY
         part_info = ANY
-        self.o.configure(task, completed_steps, steps_to_do, part_info)
-        task.put_many.assert_called_once_with(self.child, dict(
+        params = MagicMock()
+        params.filePath = "/tmp/anything.h5"
+        infos = self.o.configure(
+            task, completed_steps, steps_to_do, part_info, params)
+        self.assertIsNone(infos)
+        task.put_many_async.assert_called_once_with(self.child, dict(
             enableCallbacks=True,
             computeStatistics=True))
+        expected_filename = "/tmp/BLOCK-STAT-attributes.xml"
+        task.put_async.assert_called_once_with(
+            self.child["attributesFile"], expected_filename)
+        expected_xml = """<?xml version="1.0" ?>
+<Attributes>
+<Attribute addr="0" datatype="DOUBLE" description="Sum of the array" name="StatsTotal" source="TOTAL" type="PARAM" />
+</Attributes>"""
+        actual_xml = open(expected_filename).read().replace(">", ">\n")
+        self.assertEqual(actual_xml.splitlines(), expected_xml.splitlines())
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
