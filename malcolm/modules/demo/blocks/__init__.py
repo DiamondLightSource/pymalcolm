0c392e9d617e7abb880fc613014b927e42f22747
Andrew Wilson <andrew.wilson@diamond.ac.uk>
2017-04-27 16:59:00 +0100

    blocks/__init__.py for xmap, add check_yaml_names for some other modules

--- malcolm/modules/demo/blocks/__init__.py
+++ malcolm/modules/demo/blocks/__init__.py
@@ -1,7 +1,7 @@
-from malcolm.yamlutil import make_block_creator
+from malcolm.yamlutil import make_block_creator, check_yaml_names
 
 counter_block = make_block_creator(__file__, "counter_block.yaml")
 hello_block = make_block_creator(__file__, "hello_block.yaml")
 ticker_block = make_block_creator(__file__, "ticker_block.yaml")
 
-del make_block_creator
+check_yaml_names(globals())
--- malcolm/modules/web/blocks/__init__.py
+++ malcolm/modules/web/blocks/__init__.py
@@ -1,7 +1,7 @@
-from malcolm.yamlutil import make_block_creator
+from malcolm.yamlutil import make_block_creator, check_yaml_names
 
 web_server_block = make_block_creator(__file__, "web_server_block.yaml")
 websocket_client_block = make_block_creator(
     __file__, "websocket_client_block.yaml")
 
-del make_block_creator
+check_yaml_names(globals())
