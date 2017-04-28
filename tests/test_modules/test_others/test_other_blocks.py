b169b2678bef1409ca197bb7e2626fa87711baa3
Andrew Wilson <andrew.wilson@diamond.ac.uk>
2017-04-28 11:25:36 +0100

    Tests for BL18I

--- tests/test_modules/test_others/test_other_blocks.py
+++ tests/test_modules/test_others/test_other_blocks.py
@@ -13,8 +13,16 @@ from malcolm.modules.BL08I.blocks import i08_det_scan_combined_block,\
     i08_scan_butterfly_block,\
     i08_scan_combined_block,\
     i08_two_det_scan_combined_block
+from malcolm.modules.BL18I.blocks import i18_fine_theta_manager_block,\
+    i18_pmac_manager_block,\
+    i18_table01_manager_block,\
+    i18_table03_manager_block
+
+
 
 class TestBuiltin(unittest.TestCase):
+
+    # ADAndor Blocks
     def test_andor_detector_driver_block(self):
         controller = call_with_params(
             andor_detector_driver_block, Mock(), mri="my_mri", prefix="PV:PRE")
@@ -29,6 +37,7 @@ class TestBuiltin(unittest.TestCase):
 
         del controller
 
+    # ADPandABlocks blocks
     def test_pandablocks_manager_block(self):
         controller = call_with_params(
             pandablocks_runnable_block, Mock(), mriPrefix="mriPrefix",
@@ -36,6 +45,7 @@ class TestBuiltin(unittest.TestCase):
 
         del controller
 
+    # BL08I blocks
     def test_i08_det_scan_combined_block(self):
         controller = call_with_params(
             i08_det_scan_combined_block, Mock(), mri="mri", det="det",
@@ -46,7 +56,7 @@ class TestBuiltin(unittest.TestCase):
     def test_i08_pmac_manager_block(self):
         controller = call_with_params(
             i08_pmac_manager_block, Mock(), mriPrefix="mriPrefix",
-            configDir="/dls/tmp")
+            configDir="/tmp/malcolm")
 
         del controller
 
@@ -69,5 +79,36 @@ class TestBuiltin(unittest.TestCase):
             i08_two_det_scan_combined_block, Mock(), mri="mri", det1="det1",
             det2="det2",
             brick="brick", pandabox="pandabox", configDir="/tmp/malcolm")
+
+        del controller
+
+    # BL18I blocks
+    def test_i18_fine_theta_manager_block(self):
+        controller = call_with_params(
+            i18_fine_theta_manager_block, Mock(), mri="mri", det="det",
+            brick="brick", pandabox="pandabox", configDir="/tmp/malcolm")
+
+        del controller
+
+    def test_i18_pmac_manager_block(self):
+        controller = call_with_params(
+            i18_pmac_manager_block, Mock(), mriPrefix="mriPrefix",
+            configDir="/tmp/malcolm")
+
+        del controller
+
+    def test_i18_table01_manager_block(self):
+        controller = call_with_params(
+            i18_table01_manager_block, Mock(), mri="mri",
+            det="det", brick="brick", pandabox="pandabox",
+            configDir="/tmp/malcolm")
+
+        del controller
+
+    def test_i18_table03_manager_block(self):
+        controller = call_with_params(
+            i18_table03_manager_block, Mock(), mri="mri",
+            det="det", brick="brick", pandabox="pandabox",
+            configDir="/tmp/malcolm")
 
         del controller
