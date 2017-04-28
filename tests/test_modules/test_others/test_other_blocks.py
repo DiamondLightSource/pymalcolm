7346242df74019a2eb21edd63414e3d5bcb8ce4e
Andrew Wilson <andrew.wilson@diamond.ac.uk>
2017-04-28 10:02:18 +0100

    Tests for ADPandABlocks

--- tests/test_modules/test_others/test_other_blocks.py
+++ tests/test_modules/test_others/test_other_blocks.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from malcolm.core import call_with_params
 from malcolm.modules.ADAndor.blocks import andor_detector_driver_block,\
 andor_detector_manager_block
+from malcolm.modules.ADPandABlocks.blocks import pandablocks_runnable_block
 
 class TestBuiltin(unittest.TestCase):
     def test_andor_detector_driver_block(self):
@@ -17,7 +18,14 @@ class TestBuiltin(unittest.TestCase):
 
     def test_andor_detector_manager_block(self):
         controller = call_with_params(
-            andor_detector_manager_block, Mock(), mriPrefix="my_mri", pvPrefix="PV:PRE",
+            andor_detector_manager_block, Mock(), mriPrefix="my_mri",
+            pvPrefix="PV:PRE",
             configDir="/dls/tmp")
 
         del controller
+
+    def test_pandablocks_manager_block(self):
+        controller = call_with_params(
+            pandablocks_runnable_block, Mock(), mriPrefix="mriPrefix",
+            pvPrefix="PV:PRE", configDir="/dls/tmp"
+        )
