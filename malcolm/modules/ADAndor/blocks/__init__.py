6c2c61649e3fdb5856c14401bce434c5dcf094f0
Andrew Wilson <andrew.wilson@diamond.ac.uk>
2017-04-28 08:49:31 +0100

    Typo

--- malcolm/modules/ADAndor/blocks/__init__.py
+++ malcolm/modules/ADAndor/blocks/__init__.py
@@ -4,6 +4,6 @@ andor_detector_driver_block = make_block_creator(
     __file__, "andor_detector_driver_block.yaml")
 
 andor_detector_manager_block = make_block_creator(
-    __file__, "andor_detector_manager.block.yaml")
+    __file__, "andor_detector_manager_block.yaml")
 
 check_yaml_names(globals())
