eb3ed84bfec6281ea083e21a8ec6754b701172fa
Tom Cobb <tom.cobb@diamond.ac.uk>
2017-05-02 11:26:25 +0100

    Updated simulation

--- malcolm/modules/ADCore/parts/simdetectordriverpart.py
+++ malcolm/modules/ADCore/parts/simdetectordriverpart.py
@@ -2,8 +2,6 @@ from malcolm.modules.ADCore.parts import ExposureDetectorDriverPart
 
 
 class SimDetectorDriverPart(ExposureDetectorDriverPart):
-    def create_attributes(self):
-        for data in super(SimDetectorDriverPart, self).create_attributes():
-            yield data
-        self.trigger_mode.set_value("Software")
+    def is_hardware_triggered(self, child):
+        return False
 
