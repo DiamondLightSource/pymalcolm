fd5e21705d8f7757cf345c8c98af260203c44517
Gary Yendell <gary.yendell@diamond.ac.uk>
2017-06-30 16:52:27 +0100

    Improve import logic for clearer error messages

--- malcolm/modules/__init__.py
+++ malcolm/modules/__init__.py
@@ -1,14 +1,18 @@
 class Importer(object):
     def __init__(self):
         self.update_dict = {}
-        self.ignore = ["docs"]
+        self.dirnames = [
+            "vmetas", "infos", "controllers", "parts", "includes", "blocks"]
 
-    def import_subpackages(self, path):
+    def import_subpackages(self, path, filter=()):
         import os
         dirname = os.path.join(os.path.dirname(__file__), *path)
         for f in os.listdir(dirname):
-            if f not in self.ignore and os.path.isdir(os.path.join(dirname, f)):
-                self.try_import_path(path + [f])
+            if not filter or f in filter:
+                if os.path.isdir(os.path.join(dirname, f)):
+                    self.try_import_path(path + [f])
+                    # Try the import of subpackages too
+                    self.import_subpackages(path + [f], self.dirnames)
 
     def try_import_path(self, path):
         import importlib
@@ -20,9 +24,7 @@ class Importer(object):
             import logging
             # Create a module level logger
             log = logging.getLogger(__name__)
-            log.info("Importing %s failed", name, exc_info=True)
-        # Try the import of subpackages too
-        self.import_subpackages(path)
+            log.warning("Importing %s failed", name, exc_info=True)
 
     def prepare(self, globals_d):
         self.import_subpackages([])
