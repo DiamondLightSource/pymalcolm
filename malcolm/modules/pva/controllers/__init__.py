054122ae33249b2f3461e97d89bc0e99dffad4b6
Tom Cobb <tom.cobb@diamond.ac.uk>
2017-07-18 09:28:05 +0100

    Fix non-relative import for Py3

--- malcolm/modules/pva/controllers/__init__.py
+++ malcolm/modules/pva/controllers/__init__.py
@@ -1,4 +1,3 @@
-# Find all subpackages, MethodMeta decorated callables, and YAML files
 from .pvaclientcomms import PvaClientComms
 from .pvaservercomms import PvaServerComms
 
--- malcolm/modules/scanning/infos/__init__.py
+++ malcolm/modules/scanning/infos/__init__.py
@@ -1,4 +1,4 @@
-from parametertweakinfo import ParameterTweakInfo
+from .parametertweakinfo import ParameterTweakInfo
 
 # Expose all the classes
 __all__ = sorted(k for k, v in globals().items() if type(v) == type)
