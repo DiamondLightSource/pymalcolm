4883c3c7d2e8698dc46360205d1d5344921f1340
Tom Cobb <tom.cobb@diamond.ac.uk>
2017-08-01 10:24:57 +0100

    Moved BL14I modules into BL directory

--- malcolm/modules/BL14I/blocks/__init__.py
+++
@@ -1,7 +0,0 @@
-from malcolm.yamlutil import make_block_creator, check_yaml_names
-
-pmac_runnable_block = make_block_creator(__file__, "pmac_runnable_block.yaml")
-xspress3_scan_block = make_block_creator(__file__, "xspress3_scan_block.yaml")
-
-__all__ = check_yaml_names(globals())
-
