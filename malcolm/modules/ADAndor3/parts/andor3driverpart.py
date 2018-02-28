03c322e8b26131689333cf053d4ed1a9f192fe96
Callum Forrester <callum.forrester@diamond.ac.uk>
2018-02-28 13:11:19 +0000

    Removed Global shutter mode enforcement

--- malcolm/modules/ADAndor3/parts/andor3driverpart.py
+++ malcolm/modules/ADAndor3/parts/andor3driverpart.py
@@ -1,8 +1,7 @@
-from malcolm.modules.ADCore.parts \
-    import DetectorDriverPart, ExposureDetectorDriverPart
+from malcolm.modules.ADCore.parts import DetectorDriverPart
 from malcolm.modules.ADCore.infos import NDArrayDatasetInfo
 from malcolm.modules.scanning.controllers import RunnableController
-from numpy import float64
+
 
 class Andor3DriverPart(DetectorDriverPart):
     @RunnableController.ReportStatus
@@ -15,18 +14,13 @@ class Andor3DriverPart(DetectorDriverPart):
         fs = super(Andor3DriverPart, self).setup_detector(
             child, completed_steps, steps_to_do, params)
 
-        # Need to use global shutter mode for fine control over exposure time.
-        # Global mode does have lower framerates, so it may be worth developing
-        # support for rolling mode in the future.
-        fs.append(child.shutterMode.put_value_async("Global"))
-        child.wait_all_futures(fs)
-
         duration = params.generator.duration
         readout_time = child.readoutTime.value
         exposure = duration - readout_time
-        fs = child.exposure.put_value_async(exposure)
+        fs.append(child.exposure.put_value_async(exposure))
         child.wait_all_futures(fs)
 
         # Need to reset acquirePeriod as it's sometimes wrong
         fs = child.acquirePeriod.put_value_async(exposure + readout_time)
+
         return fs
