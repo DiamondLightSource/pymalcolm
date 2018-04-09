199e2eba46cc4cc4016fd04904d051e07731f061
Tom Cobb <tom.cobb@diamond.ac.uk>
2018-04-09 15:18:07 +0100

    Cut down excalibur VDS and make a start on the tests

--- malcolm/modules/excalibur/parts/vdswrapperpart.py
+++ malcolm/modules/excalibur/parts/vdswrapperpart.py
@@ -46,8 +46,6 @@ class VDSWrapperPart(Part):
     CREATE = "w"
     APPEND = "a"
     READ = "r"
-    ID = "/entry/NDAttributes/NDArrayUniqueId"
-    SUM = "/entry/sum/sum"
 
     required_nodes = ["/entry/detector", "/entry/sum", "/entry/NDAttributes"]
     set_bases = ["/entry/detector", "/entry/sum"]
@@ -57,29 +55,12 @@ class VDSWrapperPart(Part):
     def __init__(self, name, data_type, stripe_height, stripe_width):
         # type: (APartName, ADataType, AStripeHeight, AStripeWidth) -> None
         super(VDSWrapperPart, self).__init__(name)
-        self.current_id = None
-        self.done_when_reaches = None
-        self.generator = None
         self.fems = [1, 2, 3, 4, 5, 6]
-        self.vds_path = ""
-        self.vds = None
-        self.command = []
-        self.raw_paths = []
-        self.raw_datasets = []
         self.data_type = data_type
         self.stripe_height = stripe_height
         self.stripe_width = stripe_width
         # Hooks
         self.register_hooked(scanning.hooks.ConfigureHook, self.configure)
-        self.register_hooked((scanning.hooks.AbortHook,
-                              scanning.hooks.PostRunReadyHook),
-                             self.close_files)
-
-    @add_call_types
-    def reset(self, context):
-        # type: (scanning.hooks.AContext) -> None
-        super(VDSWrapperPart, self).reset(context)
-        self.abort(context)
 
     def setup(self, registrar):
         # type: (PartRegistrar) -> None
@@ -88,20 +69,10 @@ class VDSWrapperPart(Part):
         registrar.report(scanning.hooks.ConfigureHook.create_info(
             self.configure))
 
-    def close_files(self):
-        for file_ in self.raw_datasets + [self.vds]:
-            if file_ is not None and file_.id.valid:
-                self.log.info("Closing file %s", file_)
-                file_.close()
-        self.raw_datasets = []
-        self.vds = None
-
     # Allow CamelCase as these parameters will be serialized
     # noinspection PyPep8Naming
     @add_call_types
     def configure(self,
-                  completed_steps,  # type: scanning.hooks.ACompletedSteps
-                  steps_to_do,  # type: scanning.hooks.AStepsToDo
                   generator,  # type: scanning.hooks.AGenerator
                   fileDir,  # type: ADCore.parts.AFileDir
                   formatName="EXCALIBUR",  # type: ADCore.parts.AFormatName
@@ -109,10 +80,7 @@ class VDSWrapperPart(Part):
                   fillValue=0,  # type: AFillValue
                   ):
         # type: (...) -> scanning.hooks.UInfos
-        self.generator = generator
-        self.current_id = completed_steps
-        self.done_when_reaches = completed_steps + steps_to_do
-        self.vds_path = os.path.join(fileDir, fileTemplate % formatName)
+        vds_path = os.path.join(fileDir, fileTemplate % formatName)
         raw_file_path = fileTemplate % self.RAW_FILE_TEMPLATE.format(1)
         node_tree = list(self.default_node_tree)
         for axis in generator.axes:
@@ -120,26 +88,16 @@ class VDSWrapperPart(Part):
                 node_tree.append(base + "/{}_set".format(axis))
                 node_tree.append(base + "/{}_set_indices".format(axis))
 
-        with h5.File(self.vds_path, self.CREATE, libver="latest") as self.vds:
+        with h5.File(vds_path, self.CREATE, libver="latest") as vds:
             for node in self.required_nodes:
-                self.vds.require_group(node)
+                vds.require_group(node)
             for node in node_tree:
-                self.vds[node] = h5.ExternalLink(raw_file_path, node)
+                vds[node] = h5.ExternalLink(raw_file_path, node)
 
-            # Create placeholder id and sum datasets
-            initial_dims = tuple([1 for _ in generator.shape])
-            initial_shape = initial_dims + (1, 1)
-            max_shape = generator.shape + (1, 1)
-            self.vds.create_dataset(self.ID, initial_shape,
-                                    maxshape=max_shape, dtype="int32")
-            self.vds.create_dataset(self.SUM, initial_shape,
-                                    maxshape=max_shape, dtype="float64",
-                                    fillvalue=np.nan)
+        # Create the VDS using vdsgen
         files = [fileTemplate % self.RAW_FILE_TEMPLATE.format(fem)
                  for fem in self.fems]
         shape = generator.shape + (self.stripe_height, self.stripe_width)
-
-        # Create the VDS using vdsgen
         fgen = SubFrameVDSGenerator(
             fileDir,
             files=files,
@@ -154,15 +112,8 @@ class VDSWrapperPart(Part):
         )
         fgen.generate_vds()
 
-        # Store required attributes
-        self.raw_paths = [os.path.abspath(os.path.join(fileDir, file_))
-                          for file_ in files]
-
-        # Open the VDS
-        self.vds = h5.File(
-                self.vds_path, self.APPEND, libver="latest", swmr=True)
         # Return the dataset information
-        dataset_infos = list(_create_dataset_infos(
-            generator, fileTemplate % formatName))
+        dataset_infos = list(
+            _create_dataset_infos(generator, fileTemplate % formatName))
 
         return dataset_infos
