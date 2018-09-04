49becbd514a722ba8b2186523149a62548a7240f
Tom Cobb <tom.cobb@diamond.ac.uk>
2018-09-04 08:46:40 +0100

    Abstract out handler in GuiServerPart so PandA can override it

--- malcolm/modules/web/parts/guiserverpart.py
+++ malcolm/modules/web/parts/guiserverpart.py
@@ -24,6 +24,8 @@ class IndexHandler(StaticFileHandler):
 
 class GuiServerPart(Part):
     """Static file server to be used as a fallback. Must be last part"""
+    GuiHandler = IndexHandler
+
     def __init__(self, name="gui", path=www_dir):
         # type: (APartName, APath) -> None
         super(GuiServerPart, self).__init__(name)
@@ -35,10 +37,10 @@ class GuiServerPart(Part):
     def report_handlers(self):
         # type: () -> UHandlerInfos
         infos = [
-            # Redirect / to /gui
-            HandlerInfo(r"/", RedirectHandler, url="/gui"),
+            # Redirect / to /gui/
+            HandlerInfo(r"/", RedirectHandler, url="/gui/"),
             # Serve index.html for /gui or /details
-            HandlerInfo(r"/(gui|details).*", IndexHandler, path=self.path),
+            HandlerInfo(r"/(gui|details).*", self.GuiHandler, path=self.path),
             # Anything else should be a static file handle
             HandlerInfo(r"/(.*)", StaticFileHandler, path=self.path)
         ]
