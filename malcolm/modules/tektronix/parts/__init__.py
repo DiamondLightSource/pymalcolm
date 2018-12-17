46f740c62762d72c6c52c2283adb247532b95f9f
Bryan Robert Tester <bryan.tester@diamond.ac.uk>
2018-12-17 09:36:19 +0000

    Created Waveform2DPlot part This takes 2 waveform PVs as X and Y data, and optionally allows the limits of the plot axes to be taken from the respective HOPR & LOPR fields

---
+++ malcolm/modules/tektronix/parts/__init__.py
@@ -0,0 +1,4 @@
+from malcolm.core import submodule_all
+from .waveformplotterpart import Waveform2DPart
+
+__all__ = submodule_all(globals())
---
+++ malcolm/modules/tektronix/parts/waveformplotterpart.py
@@ -0,0 +1,33 @@
+from malcolm.core import Part, Widget, PartRegistrar, NumberArrayMeta, DEFAULT_TIMEOUT
+from malcolm.modules.ca.util import catools
+from .. import util
+
+
+class Waveform2DPart(Part):
+    """Defines a float64[] `Attribute` that talks to a DBR_DOUBLE waveform PV,
+     and optionally talks to a second waveform to obtain x axis points
+      plus checks HOPR & LOPR on these PVs to determine axis limits"""
+
+    def __init__(self,
+                 name,  # type: util.APartName
+                 description,  # type: util.AMetaDescription
+                 yData="",  # type: util.APv
+                 xData="",  # type: util.ARbv
+                 min_delta=0.05,  # type: util.AMinDelta
+                 timeout=DEFAULT_TIMEOUT,  # type: util.ATimeout
+                 sink_port=None,  # type: util.ASinkPort
+                 widget=Widget.PLOT,  # type: util.AWidget
+                 group=None,  # type: util.AGroup
+                 config=True,  # type: util.AConfig
+                 limits_from_pv=False  # type: util.AGetLimits
+                 ):
+        # type: (...) -> None
+        super(Waveform2DPart, self).__init__(name)
+        self.caa = util.Waveform2DAttribute(
+            NumberArrayMeta("float64", description), catools.DBR_DOUBLE,
+            yData, xData, min_delta, timeout, sink_port, widget, group,
+            config, limits_from_pv)
+
+    def setup(self, registrar):
+        # type: (PartRegistrar) -> None
+        self.caa.setup(registrar, self.name, self.register_hooked)
