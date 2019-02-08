f394afb4b01b11eee97d22819fa6ac2cad5828f7
Giles Knap <giles.knap@diamond.ac.uk>
2019-02-08 11:11:20 +0000

    fix tests that used when_value_matches

--- tests/test_modules/test_ADAndor/test_andordriverpart.py
+++ tests/test_modules/test_ADAndor/test_andordriverpart.py
@@ -47,4 +47,5 @@ class TestAndorDetectorDriverPart(ChildTestCase):
             call.put('imageMode', 'Multiple'),
             call.put('numImages', 6000000),
             call.put('acquirePeriod', 0.1 - 5e-6),
-            call.post('start')]
+            call.post('start'),
+            call.when_values_matches('acquiring', True, None, 10.0, None)]
