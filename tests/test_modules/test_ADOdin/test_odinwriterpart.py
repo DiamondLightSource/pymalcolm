bf92c71d875c5093b41c6060823802bf76c7a962
Giles Knap <giles.knap@diamond.ac.uk>
2019-03-08 14:44:42 +0000

    data vds test working for 60,000 points

--- tests/test_modules/test_ADOdin/test_odinwriterpart.py
+++ tests/test_modules/test_ADOdin/test_odinwriterpart.py
@@ -1,5 +1,7 @@
 import pytest
 from mock import call, MagicMock
+import os
+from datetime import datetime
 
 from scanpointgenerator import LineGenerator, CompoundGenerator
 from malcolm.core import Context, Process
@@ -19,14 +21,17 @@ class TestOdinDWriterPart(ChildTestCase):
         self.child = self.create_child_block(
             odin_writer_block, self.process,
             mri="mri", prefix="prefix")
+        self.set_attributes(self.child,
+                            numProcesses=4)
         self.o = OdinWriterPart(name="m", mri="mri")
         self.context.set_notify_dispatch_request(self.o.notify_dispatch_request)
         self.process.start()
 
         self.completed_steps = 0
-        self.steps_to_do = 2000 * 3000
-        xs = LineGenerator("x", "mm", 0.0, 0.5, 3000, alternate=True)
-        ys = LineGenerator("y", "mm", 0.0, 0.1, 2000)
+        cols, rows = 300, 200
+        self.steps_to_do = cols * rows
+        xs = LineGenerator("x", "mm", 0.0, 0.5, cols, alternate=True)
+        ys = LineGenerator("y", "mm", 0.0, 0.1, rows)
         self.generator = CompoundGenerator([ys, xs], [], [], 0.1)
         self.generator.prepare()
 
@@ -34,15 +39,33 @@ class TestOdinDWriterPart(ChildTestCase):
         self.process.stop(timeout=1)
 
     def test_configure(self):
+        # the test makes /tmp/odin.hdf - make sure a previous one is not there
+        vds_file = os.path.join('/tmp', 'odin.hdf')
+        if os.path.exists(vds_file):
+            os.remove(vds_file)
+        # also the VDS will look for the existence of these files:
+        for i in range(1, 5):
+            # todo this is a bug since these files wont be created
+            #  in normal conditions. Need to get vdsgen to cope with this
+            #  in some fashion
+            # todo ALSO need to get vdsgen to cope with 6 million points
+            #  this will require spawning to another process methinks
+            open(os.path.join(
+                '/tmp', 'odin_raw_data{}.hdf'.format(i)), 'w'
+            ).close()
+
+        start_time = datetime.now()
         self.o.configure(
             self.context, self.completed_steps, self.steps_to_do,
             generator=self.generator, fileDir='/tmp', fileName='odin.hdf')
         assert self.child.handled_requests.mock_calls == [
-            call.put('fileName', 'odin.hdf'),
+            call.put('fileName', 'odin_raw_data.hdf'),
             call.put('filePath', '/tmp/'),
             call.put('numCapture', self.steps_to_do),
             call.post('start')]
         print(self.child.handled_requests.mock_calls)
+        print('OdinWriter configure {} points took {} secs'.format(
+            self.steps_to_do, datetime.now() - start_time))
 
     def test_run(self):
         self.o.configure(
@@ -55,7 +78,8 @@ class TestOdinDWriterPart(ChildTestCase):
             self.o.done_when_reaches)
         self.o.run(self.context)
         assert self.child.handled_requests.mock_calls == [
-            call.when_values_matches('numCaptured', 6000000, None, None, 60)]
+            call.when_values_matches(
+                'numCaptured', self.steps_to_do, None, None, 60)]
         assert self.o.registrar.report.called_once
         assert self.o.registrar.report.call_args_list[0][0][0].steps == \
                self.steps_to_do
