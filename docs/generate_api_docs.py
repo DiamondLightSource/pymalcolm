47be86e52fd319f415b6fb2061de5bc745fdbbea
Tom Cobb <tom.cobb@diamond.ac.uk>
2019-04-02 14:45:15 +0100

    Rewrote api docs generation so it only regenerates if different

--- docs/generate_api_docs.py
+++ docs/generate_api_docs.py
@@ -7,84 +7,119 @@ repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 def generate_docs():
     build_dir = os.path.join(repo_root, "docs", "build")
-    if os.path.isdir(build_dir):
-        shutil.rmtree(build_dir)
-    os.mkdir(build_dir)
+    if not os.path.exists(build_dir):
+        os.makedirs(build_dir)
 
-    # open the .rst file
-    fname = os.path.join(repo_root, "docs", "build", "modules_api.rst")
-    with open(fname, "w") as api_docs:
+    # Add entries for each module
+    modules = []
+    modules_root = os.path.join(repo_root, "malcolm", "modules")
+    for modulename in sorted(os.listdir(modules_root)):
+        module_root = os.path.join(modules_root, modulename)
+        if not os.path.isdir(module_root):
+            continue
+        # Make sure the directory exists
+        docs_build = os.path.join(repo_root, "docs", "build", modulename)
+        if not os.path.exists(docs_build):
+            os.makedirs(docs_build)
+        # Walk the tree finding documents
+        # {docname: section}
+        documents = {}
+        dirs = sorted(os.listdir(module_root))
+        # Make any parameters and defines docs
+        for fname in ["parameters.py", "defines.py", "hooks.py", "infos.py",
+                      "util.py", "blocks", "includes", "controllers", "parts"]:
+            if fname in dirs:
+                if fname.endswith(".py"):
+                    fname = fname[:-3]
+                docname = "%s_api" % fname
+                section = "malcolm.modules.%s.%s" % (modulename, fname)
+                documents[docname] = section
 
-        # add header
-        api_docs.write("malcolm.modules\n")
-        api_docs.write("===============\n\n")
-        api_docs.write(".. module:: malcolm.modules\n\n")
-
-        modules_root = os.path.join(repo_root, "malcolm", "modules")
-
-        # Add the toctree
-        api_docs.write(".. toctree::\n")
-        api_docs.write("    :maxdepth: 1\n\n")
-
-        # Add entries for each module
-        for modulename in sorted(os.listdir(modules_root)):
-            module_root = os.path.join(modules_root, modulename)
-            if not os.path.isdir(module_root):
-                continue
-            # Copy the docs dir if it exists
-            docs_build = os.path.join(repo_root, "docs", "build", modulename)
-            docs_dir = os.path.join(module_root, "docs")
-            if os.path.isdir(docs_dir):
-                shutil.copytree(docs_dir, docs_build)
-                documents = [
-                    x[:-4] for x in os.listdir(docs_dir) if x.endswith(".rst")]
+        # Copy from the docs dir if it exists
+        docs_dir = os.path.join(module_root, "docs")
+        for docname, section in sorted(documents.items()):
+            src_path = os.path.join(docs_dir, docname + ".rst")
+            rst_path = os.path.join(docs_build, docname + ".rst")
+            if os.path.exists(src_path):
+                with open(src_path) as rst:
+                    text = rst.read()
+                write_if_different(rst_path, text)
             else:
-                os.mkdir(docs_build)
-                documents = []
-            dirs = sorted(os.listdir(module_root))
-            # Make any parameters and defines docs
-            for fname in ["parameters.py", "defines.py", "hooks.py", "infos.py",
-                          "util.py"]:
-                docname = "%s_api" % fname[:-3]
-                if fname in dirs and docname not in documents:
-                    # Make document for module
-                    section = "malcolm.modules.%s.%s" % (modulename, fname[:-3])
-                    make_automodule_doc(section, docs_build)
-                    documents.append(docname)
-            for dirname in ["blocks", "includes", "controllers", "parts"]:
-                docname = "%s_api" % dirname
-                if dirname in dirs and docname not in documents:
-                    # Make document for module
-                    section = "malcolm.modules.%s.%s" % (modulename, dirname)
-                    make_automodule_doc(section, docs_build)
-                    documents.append(docname)
-            # Make the index if it doesn't exist
-            if documents and "index" not in documents:
-                make_index_doc(modulename, docs_build, documents)
-            # Add to top level page
-            if documents:
-                api_docs.write("    %s/index\n" % modulename)
+                # Or make one up
+                make_automodule_doc(section, rst_path)
+
+        # Add an index
+        src_path = os.path.join(docs_dir, "index.rst")
+        rst_path = os.path.join(docs_build, "index.rst")
+        got_index = True
+        if os.path.exists(src_path):
+            with open(src_path) as rst:
+                text = rst.read()
+            write_if_different(rst_path, text)
+        elif documents:
+            # Or make one up
+            make_index_doc(modulename, rst_path, documents)
+        else:
+            got_index = False
+
+        # Add entry to top level page
+        if got_index:
+            modules.append(modulename)
+
+    text = """
+malcolm.modules
+===============
+
+.. module:: malcolm.modules
+
+.. toctree::
+    :maxdepth: 1
+   
+"""
+    for modulename in modules:
+        text += "    %s/index\n" % modulename
+
+    fname = os.path.join(repo_root, "docs", "build", "modules_api.rst")
+    write_if_different(fname, text)
 
 
-def make_automodule_doc(section, docs_build):
-    docname = section.rsplit(".")[-1]
-    with open(os.path.join(docs_build, docname + "_api.rst"), "w") as f:
-        f.write(section + "\n")
-        f.write("=" * len(section) + "\n\n")
-        f.write(".. automodule:: %s\n" % section)
-        f.write("    :members:\n")
+def make_automodule_doc(section, rst_path):
+    text = """
+%(section)s
+%(underline)s
+
+.. automodule:: %(section)s
+    :members:
+""" % dict(section=section, underline="=" * len(section))
+
+    write_if_different(rst_path, text)
 
 
-def make_index_doc(modulename, docs_build, doc_dirs):
-    with open(os.path.join(docs_build, "index.rst"), "w") as f:
-        f.write(modulename + "\n")
-        f.write("=" * len(modulename) + "\n\n")
-        f.write(".. module:: malcolm.modules.%s\n\n" % modulename)
-        f.write(".. toctree::\n")
-        f.write("    :maxdepth: 1\n")
-        f.write("    :caption: malcolm.modules.%s\n\n" % modulename)
-        for doc in doc_dirs:
-            f.write("    %s <%s>\n" % (doc[:-4], doc))
+def make_index_doc(modulename, rst_path, documents):
+    text = """
+%(modulename)s
+%(underline)s
+
+.. module:: malcolm.modules.%(modulename)s
+
+.. toctree::
+    :maxdepth: 1
+    :caption: malcolm.modules.%(modulename)s
+
+""" % dict(modulename=modulename, underline="=" * len(modulename))
+
+    for doc in sorted(documents):
+        text += "    %s <%s.rst>\n" % (doc, doc)
+
+    write_if_different(rst_path, text)
+
+
+def write_if_different(path, text):
+    with open(path) as current:
+        different = current.read() != text
+    if different:
+        with open(path, "w") as updated:
+            updated.write(text)    
 
 
 if __name__ == "__main__":
