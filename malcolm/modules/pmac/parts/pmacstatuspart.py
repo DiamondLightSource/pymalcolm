3abf351de0137d3f2b9cad692f9b1bb857a74ef3
Tom Cobb <tom.cobb@diamond.ac.uk>
2019-04-03 11:59:36 +0100

    Fix PmacStatusPart to use registrar context

--- malcolm/modules/pmac/parts/pmacstatuspart.py
+++ malcolm/modules/pmac/parts/pmacstatuspart.py
@@ -9,9 +9,6 @@ with Anno("The Servo Frequency of the PMAC in Hz"):
 
 
 class PmacStatusPart(builtin.parts.ChildPart):
-    # The context we will use for all our functions
-    context = None
-
     def setup(self, registrar):
         # type: (PartRegistrar) -> None
         super(PmacStatusPart, self).setup(registrar)
@@ -19,16 +16,9 @@ class PmacStatusPart(builtin.parts.ChildPart):
         registrar.add_method_model(self.servo_frequency, "servoFrequency")
 
     @add_call_types
-    def init(self, context):
-        # type: (builtin.hooks.AContext) -> None
-        # Store the context for later use
-        self.context = context
-        super(PmacStatusPart, self).init(context)
-
-    @add_call_types
     def servo_frequency(self):
         # type: () -> AServoFrequency
-        child = self.context.block_view(self.mri)
+        child = self.registrar.context.block_view(self.mri)
         freq = 8388608000. / child.i10.value
         return freq
 
--- tests/test_modules/test_pmac/test_pmacstatuspart.py
+++ tests/test_modules/test_pmac/test_pmacstatuspart.py
@@ -1,6 +1,7 @@
 from mock import Mock
 
 from malcolm.core import Context, Process, PartRegistrar
+from malcolm.modules.builtin.controllers import ManagerController
 from malcolm.modules.pmac.parts import PmacStatusPart
 from malcolm.modules.pmac.blocks import pmac_status_block
 from malcolm.testutil import ChildTestCase
@@ -10,25 +11,21 @@ class TestPmacStatusPart(ChildTestCase):
 
     def setUp(self):
         self.process = Process("Process")
-        self.context = Context(self.process)
         child = self.create_child_block(
             pmac_status_block, self.process, mri="my_mri", prefix="PV:PRE")
         self.set_attributes(child,
                             i10=1705244)
-        self.o = PmacStatusPart(name="part", mri="my_mri")
-        self.context.set_notify_dispatch_request(self.o.notify_dispatch_request)
+        c = ManagerController("PMAC", "/tmp")
+        self.o = PmacStatusPart(
+            name="part", mri="my_mri", initial_visibility=True)
+        c.add_part(self.o)
+        self.process.add_controller(c)
         self.process.start()
-        self.o.init(self.context)
+        self.b = c.block_view()
 
     def tearDown(self):
         self.process.stop(timeout=1)
 
-    def test_setup(self):
-        registrar = Mock(spec=PartRegistrar)
-        self.o.setup(registrar)
-        registrar.add_method_model.assert_called_once_with(
-            self.o.servo_frequency, "servoFrequency")
-
     def test_servo_freq(self):
-        freq = self.o.servo_frequency()
+        freq = self.b.servoFrequency()
         assert freq == 4919.300698316487
