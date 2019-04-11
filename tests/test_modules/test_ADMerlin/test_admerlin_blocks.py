193265b5d8f81fa3d02d298e68ec8e4070befe1d
Tom Cobb <tom.cobb@diamond.ac.uk>
2019-04-11 10:40:08 +0100

    Added label to ADMerlin

--- tests/test_modules/test_ADMerlin/test_admerlin_blocks.py
+++ tests/test_modules/test_ADMerlin/test_admerlin_blocks.py
@@ -6,6 +6,7 @@ from malcolm.modules.ADMerlin.blocks import merlin_runnable_block
 
 class TestADMerlinBlocks(ChildTestCase):
     def test_merlin_detector_runnable_block(self):
-        self.create_child_block(
+        c = self.create_child_block(
             merlin_runnable_block, Mock(),
             mri_prefix="mri_prefix", pv_prefix="pv_prefix", config_dir="/tmp")
+        assert c.parts["label"].initial_value == "Merlin"
