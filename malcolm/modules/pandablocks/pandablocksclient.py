3671f94832df4e9fcb4d320ea0539687bff5fc23
Tom Cobb <tom.cobb@diamond.ac.uk>
2019-04-29 15:23:42 +0100

    Added get_pcap_bits_fields to PandA client

--- malcolm/modules/pandablocks/pandablocksclient.py
+++ malcolm/modules/pandablocks/pandablocksclient.py
@@ -242,7 +242,7 @@ class PandABlocksClient(object):
                 if field_type in ("bit_mux", "pos_mux") or field_subtype == \
                         "enum":
                     enum_fields.append(field_name)
-                elif field_type in ("pos_out", "ext_out"):
+                elif field_type == "ext_out":
                     enum_fields.append(field_name + ".CAPTURE")
             enum_queues = self.parameterized_send(
                 "*ENUMS.%s.%%s?\n" % block_name, enum_fields)
@@ -261,6 +261,22 @@ class PandABlocksClient(object):
                     field_type, field_subtype, description, labels)
 
         return blocks
+
+    def get_pcap_bits_fields(self):
+        # {field_to_set: [bit_names]}
+        # E.g. {"PCAP.BITS0"=["TTLIN1.VAL", "TTLIN2.VAL", ...], ...}
+        bits_fields = []
+        for line in self.send_recv("PCAP.*?\n"):
+            split = line.split()
+            if len(split) == 4:
+                field_name, _, field_type, field_subtype = split
+                if field_type == "ext_out" and field_subtype == "bits":
+                    bits_fields.append("PCAP.%s" % field_name)
+        bits_queues = self.parameterized_send("%s.BITS?\n", sorted(bits_fields))
+        bits = OrderedDict()
+        for k, queue in bits_queues.items():
+            bits[k] = self.recv(queue)
+        return bits
 
     def get_changes(self):
         table_queues = {}
@@ -317,13 +333,21 @@ class PandABlocksClient(object):
             return strip_ok(resp)
 
     def set_field(self, block, field, value):
-        try:
-            resp = self.send_recv("%s.%s=%s\n" % (block, field, value))
-        except ValueError as e:
-            raise ValueError("Error setting %s.%s to %r: %s" % (
-                block, field, value, e))
-        else:
-            assert resp == "OK", "Expected OK, got %r" % resp
+        self.set_fields({"%s.%s" % (block, field): value})
+
+    def set_fields(self, field_values):
+        queues = OrderedDict()
+        for field, value in field_values.items():
+            message = "%s=%s\n" % (field, value)
+            queues[(field, value)] = self.send(message)
+        for (field, value), queue in queues.items():
+            try:
+                resp = self.recv(queue)
+            except ValueError as e:
+                raise ValueError(
+                    "Error setting %s to %r: %s" % (field, value, e))
+            else:
+                assert resp == "OK", "Expected OK, got %r" % resp
 
     def set_table(self, block, field, int_values):
         lines = ["%s.%s<\n" % (block, field)]
