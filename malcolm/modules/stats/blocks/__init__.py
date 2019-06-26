f1acedcbfa198ee9c52e352ca941e11de97f9fd7
Bryan Robert Tester <bryan.tester@diamond.ac.uk>
2019-06-26 12:57:15 +0100

    fixed naming of stats_block creator

--- malcolm/modules/stats/blocks/__init__.py
+++ malcolm/modules/stats/blocks/__init__.py
@@ -1,5 +1,5 @@
 from malcolm.yamlutil import make_block_creator, check_yaml_names
 
-dev_malcolm_stats_block = make_block_creator(__file__, "stats_block.yaml")
+stats_block = make_block_creator(__file__, "stats_block.yaml")
 
 __all__ = check_yaml_names(globals())
