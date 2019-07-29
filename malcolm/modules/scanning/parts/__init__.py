0505b51b3d2e2be4d75c6a2ff69624cd75d239b1
Benjamin Bradnick <ben.bradnick@diamond.ac.uk>
2019-07-29 16:26:52 +0100

    Updated __init__.py to allow use of MinTurnaroundParts

--- malcolm/modules/scanning/parts/__init__.py
+++ malcolm/modules/scanning/parts/__init__.py
@@ -2,6 +2,7 @@ from .datasettablepart import DatasetTablePart
 from .detectorchildpart import DetectorChildPart, AMri, ADetectorTable, \
     AInitialVisibility, APartName
 from .simultaneousaxespart import SimultaneousAxesPart, USimultaneousAxes
+from .minturnaroundpart import MinTurnaroundPart
 
 # Expose a nice namespace
 from malcolm.core import submodule_all
