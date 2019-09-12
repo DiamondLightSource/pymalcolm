093f8f26efb4d9dbfa40cddda28428a61e704175
Bryan Robert Tester <bryan.tester@diamond.ac.uk>
2019-09-12 10:31:13 +0100

    replaced stats collection with stats_block_w_servers

--- malcolm/modules/stats/blocks/__init__.py
+++ malcolm/modules/stats/blocks/__init__.py
@@ -1,6 +1,7 @@
 from malcolm.yamlutil import make_block_creator, check_yaml_names
 
 stats_block = make_block_creator(__file__, "stats_block.yaml")
+stats_block_w_servers = make_block_creator(__file__, "stats_block_w_servers.yaml")
 ioc_status_block = make_block_creator(__file__, "ioc_status_block.yaml")
 wrapper_block = make_block_creator(__file__, "wrapper_block.yaml")
 
--- malcolm/modules/stats/includes/__init__.py
+++
@@ -1,5 +0,0 @@
-from malcolm.yamlutil import make_include_creator, check_yaml_names
-
-stats_collection = make_include_creator(__file__, "stats_collection.yaml")
-
-__all__ = check_yaml_names(globals())
