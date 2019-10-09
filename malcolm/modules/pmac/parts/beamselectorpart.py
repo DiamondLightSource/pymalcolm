26635dd79240b295860c8808b035729f6f4ad88e
Dino Tahirovic <elvedin.tahirovic@diamond.ac.uk>
2019-10-09 12:56:08 +0100

    Added a unit test for Beam Selector part.

--- malcolm/modules/pmac/parts/beamselectorpart.py
+++ malcolm/modules/pmac/parts/beamselectorpart.py
@@ -1,6 +1,6 @@
 from annotypes import Anno, add_call_types, TYPE_CHECKING
 
-from malcolm.core import PartRegistrar
+from malcolm.core import PartRegistrar, NumberMeta, config_tag, Widget
 from malcolm.modules import builtin
 
 # Pull re-used annotypes into our namespace in case we are subclassed
@@ -8,23 +8,46 @@ APartName = builtin.parts.APartName
 AMri = builtin.parts.AMri
 AIV = builtin.parts.AInitialVisibility
 
+with Anno("The demand value to move our counter motor to"):
+    ADemand = float
+
 # TODO Type checking?
-# TODO add call types
 
 class BeamSelectorPart(builtin.parts.ChildPart):
 
     def __init__(self,
                  name, # type: APartName
-                 mri, # type: AMri
-                 initial_visibility = None # type: AIV
+                 mri, # type: AMri,
+                 initial_visibility = False # type: AIV
                  ):
         # type: (...) -> None
 
-        pass
+        super(BeamSelectorPart, self).__init__(name,
+                                               mri,
+                                               stateful=False,
+                                               initial_visibility=True)
 
     def setup(self, registrar):
         # type: (PartRegistrar) -> None
 
-        pass
+        super(BeamSelectorPart, self).setup(registrar)
 
+        self.angle = NumberMeta("float64",
+                                "The current angle of beam selector",
+                                tags=[config_tag(),
+                                      Widget.TEXTINPUT.tag()]
+                                ).create_attribute_model()
+        registrar.add_attribute_model("angle",
+                                      self.angle,
+                                      self.angle.set_value)
 
+        registrar.add_method_model(self.move,
+                                   self.name,
+                                   needs_context=True)
+
+    @add_call_types
+    def move(self, context, angle):
+        # type: (builtin.hooks.AContext, ADemand) -> None
+
+        self.angle.set_value(self.angle.value)
+        child = None
