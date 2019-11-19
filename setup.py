3526739fe7a7a42b6b4f36a75f7a9025decfac65
Dino Tahirovic <elvedin.tahirovic@diamond.ac.uk>
2019-11-19 18:32:45 +0000

    Use the new scanpointgenerator release.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ install_requires = [
     # DLS developed
     "annotypes==0.20",
     "cothread==2.16",
-    "scanpointgenerator==2.3",
+    "scanpointgenerator==3.0",
     "vdsgen==0.5.2",
     ]
 
