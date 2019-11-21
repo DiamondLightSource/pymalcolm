19c8ce8995fe7f54d76b1ccfd6603b7ca73b8e19
Bryan Robert Tester <bryan.tester@diamond.ac.uk>
2019-11-21 17:12:35 +0000

    pulled in v1.7.7 of web GUI (bug fixes; navbar + colours)

--- malcolm/modules/web/update_malcolmjs.sh
+++ malcolm/modules/web/update_malcolmjs.sh
@@ -2,7 +2,7 @@
 HERE=$(dirname $0)
 
 # Pull in changes from a built version of malcolmjs
-RELEASE=https://github.com/dls-controls/malcolmjs/releases/download/1.7.6/malcolmjs-1.7.6-0-g5f441da.tar.gz
+RELEASE=https://github.com/dls-controls/malcolmjs/releases/download/1.7.7/malcolmjs-1.7.7-0-g728afb8.tar.gz
 TMP=/tmp/malcolmjs.tar.gz
 
 git rm -rf $HERE/www
