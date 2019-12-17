22ed435f77256da299e9bf0f20e4ed1aac8a0f80
Dino Tahirovic <elvedin.tahirovic@diamond.ac.uk>
2019-12-17 17:53:59 +0000

    Unit test for on_validate()

--- tests/test_modules/test_ADPandABlocks/test_pandaalternatingdivpart.py
+++ tests/test_modules/test_ADPandABlocks/test_pandaalternatingdivpart.py
@@ -3,6 +3,7 @@ from malcolm.modules.ADPandABlocks.parts import PandAAlternatingDivPart
 from malcolm.modules.ADPandABlocks.blocks import panda_alternating_div_block
 from malcolm.core import Context, Process
 from malcolm.modules.builtin.controllers import ManagerController
+from malcolm.modules.scanning.util import DetectorTable
 
 class TestPandAAlternatingDivPart(ChildTestCase):
     def setUp(self):
@@ -10,22 +11,26 @@ class TestPandAAlternatingDivPart(ChildTestCase):
         self.process = Process()
         self.context = Context(self.process)
 
-        self.panda = ManagerController("PANDA", "/tmp")
+        self.panda = ManagerController("ML-PANDA-01", "/tmp")
         #self.process.add_controller(self.panda)
 
         self.child = self.create_child_block(panda_alternating_div_block,
                                              self.process,
-                                             mri="PANDA",
-                                             panda="panda")
+                                             mri="ML-PANDA-01",
+                                             panda="ML-PANDA-01")
 
         self.part_under_test = \
-            PandAAlternatingDivPart("alternatingDivPart",
-                                    "PANDA")
+            PandAAlternatingDivPart(name="alternatingDivPart",
+                                    mri="ML-PANDA-01")
 
     def tearDown(self):
         pass
 
     def test_validate(self):
-        detector_table = None
+        detector_table = DetectorTable([True],
+                                       ["PandA"],
+                                       ["ML-PANDA-01"],
+                                       [1.0],
+                                       [2])
         self.part_under_test.on_validate(context=self.context,
                                          detectors=detector_table)
