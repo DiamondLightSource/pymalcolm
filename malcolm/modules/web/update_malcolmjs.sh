83fc16343fc5186f857f187b1e2e60c94e80b6c6
Bryan T <blondejamtart@gmail.com>
2019-12-20 10:42:54 +0000

    updated web gui to 1.7.9 (editable meter + table row seek)

--- malcolm/modules/web/update_malcolmjs.sh
+++ malcolm/modules/web/update_malcolmjs.sh
@@ -2,7 +2,7 @@
 HERE=$(dirname $0)
 
 # Pull in changes from a built version of malcolmjs
-RELEASE=https://github.com/dls-controls/malcolmjs/releases/download/1.7.8/malcolmjs-1.7.8-0-g794814a.tar.gz
+RELEASE=https://github.com/dls-controls/malcolmjs/releases/download/1.7.9/malcolmjs-1.7.9-0-g68b3fac.tar.gz
 TMP=/tmp/malcolmjs.tar.gz
 
 git rm -rf $HERE/www
