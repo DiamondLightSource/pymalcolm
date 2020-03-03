0de2c1369e356f74a2ea43617ddd9f30d60fc647
Dino Tahirovic <elvedin.tahirovic@diamond.ac.uk>
2020-03-03 15:29:59 +0000

    ADPilatus yaml name check pass

--- malcolm/modules/ADPilatus/blocks/__init__.py
+++ malcolm/modules/ADPilatus/blocks/__init__.py
@@ -1,8 +1,8 @@
 from malcolm.yamlutil import make_block_creator, check_yaml_names
 
-adpilatus_driver_block = make_block_creator(
-    __file__, " ADPilatus_driver_block.yaml")
-adpilatus_runnable_block = make_block_creator(
+ADPilatus_driver_block = make_block_creator(
+    __file__, "ADPilatus_driver_block.yaml")
+ADPilatus_runnable_block = make_block_creator(
     __file__, "ADPilatus_runnable_block.yaml")
 
 __all__ = check_yaml_names(globals())
