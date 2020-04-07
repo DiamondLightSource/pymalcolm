5af86f1e7f68fa4a08c3e7bef6b3a5d682d89219
Benjamin Bradnick <ben.bradnick@diamond.ac.uk>
2020-04-07 09:45:45 +0100

    pvaservercomms: add test for makeChannel

--- tests/test_modules/test_pva/test_pvaservercomms.py
+++ tests/test_modules/test_pva/test_pvaservercomms.py
@@ -1,4 +1,5 @@
 import unittest
+from mock import MagicMock
 
 from malcolm.modules.pva.controllers import PvaServerComms
 
@@ -44,3 +45,18 @@ class TestPvaServerComms(unittest.TestCase):
 
         self.assertRaises(
             NameError, self.pva_server_comms._make_channel, channel_name, source)
+
+    def test_makeChannel_returns_CallbackResult(self):
+        # Add channel
+        channel_name = "CHANNEL1"
+        source = "Source1"
+        self.pva_server_comms._published.add(channel_name)
+        # Mock make_channel method
+        mock_make_channel = MagicMock(name="mock_make_channel")
+        mock_make_channel.return_value = "return_value"
+        self.pva_server_comms._make_channel = mock_make_channel
+
+        result = self.pva_server_comms.makeChannel(channel_name, source)
+
+        self.assertEqual("return_value", result)
+        mock_make_channel.assert_called_once_with(channel_name, source)
