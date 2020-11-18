7e67361b13559896f35f1a1f86e97ac8b5dafba5
EdWarrick <edmund.warrick@diamond.ac.uk>
2020-11-18 10:18:07 +0000

    Removed syslog handler to prevent excessive logging to local disk

--- malcolm/imalcolm.py
+++ malcolm/imalcolm.py
@@ -65,9 +65,9 @@ def make_logging_config(args):
                 "format": "%(asctime)s - %(levelname)6s - %(name)s\n"
                           "    %(message)s"
             },
-            "syslog": {
-                "format": "%(name)s: %(message)s\n##%(extra)s##"
-            },
+            #"syslog": {
+            #    "format": "%(name)s: %(message)s\n##%(extra)s##"
+            #},
         },
 
         "handlers": {
@@ -88,12 +88,12 @@ def make_logging_config(args):
             #    "encoding": "utf8"
             #},
 
-            "syslog_graylog": {
-                "class": "malcolm.syslogger.JsonSysLogHandler",
-                "formatter": "syslog",
-                "address": "/dev/log",
-                "facility": "local0"
-            },
+            #"syslog_graylog": {
+            #    "class": "malcolm.syslogger.JsonSysLogHandler",
+            #    "formatter": "syslog",
+            #    "address": "/dev/log",
+            #    "facility": "local0"
+            #},
 
             "graylog_gelf": {
                 "class": "pygelf.GelfTcpHandler",
@@ -129,7 +129,7 @@ def make_logging_config(args):
 
         "root": {
             "level": "DEBUG",
-            "handlers": ["graylog_gelf", "console", "syslog_graylog"],
+            "handlers": ["graylog_gelf", "console"]#, "syslog_graylog"],
         }
     }
 
