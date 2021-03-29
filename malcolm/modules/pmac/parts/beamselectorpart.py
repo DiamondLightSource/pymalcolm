3cde648431d6d268798d2b923caf842b4ee21baa
Ben Bradnick <ben.bradnick@diamond.ac.uk>
2021-03-29 14:53:37 +0100

    BeamSelectorPart: refactoring and add validate method

--- malcolm/modules/pmac/parts/beamselectorpart.py
+++ malcolm/modules/pmac/parts/beamselectorpart.py
@@ -1,7 +1,12 @@
 from typing import Tuple
 
 from annotypes import Anno, add_call_types
-from scanpointgenerator import CompoundGenerator, LineGenerator, StaticPointGenerator
+from scanpointgenerator import (
+    CompoundGenerator,
+    Generator,
+    LineGenerator,
+    StaticPointGenerator,
+)
 
 from malcolm.modules import builtin, scanning
 from malcolm.modules.scanning.util import ADetectorTable
@@ -35,10 +40,10 @@ class BeamSelectorPart(PmacChildPart):
         name: APartName,
         mri: AMri,
         selector_axis: ASelectorAxis,
-        tomo_angle: ATomoAngle,
-        diff_angle: ADiffAngle,
-        tomo_detector: ATomoDetector,
-        diff_detector: ADiffDetector,
+        imaging_angle: ATomoAngle,
+        diffraction_angle: ADiffAngle,
+        imaging_detector: ATomoDetector,
+        diffraction_detector: ADiffDetector,
         move_time: AMoveTime,
         initial_visibility: AIV = False,
     ) -> None:
@@ -48,69 +53,95 @@ class BeamSelectorPart(PmacChildPart):
             raise ValueError("Move time must be larger than zero.")
         elif not isinstance(selector_axis, str):
             raise ValueError("Selector axis name must be a string")
-        elif not isinstance(tomo_detector, str):
+        elif not isinstance(imaging_detector, str):
             raise ValueError("Tomography detector name must be a string")
-        elif not isinstance(diff_detector, str):
+        elif not isinstance(diffraction_detector, str):
             raise ValueError("Diffraction detector name must be a string")
 
         super().__init__(name, mri, initial_visibility)
         self.selector_axis = selector_axis
-        self.tomo_detector = tomo_detector
-        self.diff_detector = diff_detector
-        self.tomo_angle = float(tomo_angle)
-        self.diff_angle = float(diff_angle)
+        self.imaging_detector = imaging_detector
+        self.diffraction_detector = diffraction_detector
+        self.imaging_angle = float(imaging_angle)
+        self.diffraction_angle = float(diffraction_angle)
         self.move_time = float(move_time)
+
+    def _get_error_message(self, name: str, mri: str, message: str) -> str:
+        return f"{mri} (name {name}): {message}"
+
+    def _check_detector_parameters(
+        self, name: str, mri: str, frames: int, enable: bool, exposure: float
+    ) -> None:
+        if frames != 1:
+            raise ValueError(
+                self._get_error_message(name, mri, "Can only do 1 frame per step")
+            )
+        elif enable is not True:
+            raise ValueError(
+                self._get_error_message(name, mri, "Detector needs to be enabled")
+            )
+        elif exposure <= 0.0:
+            raise ValueError(
+                self._get_error_message(
+                    name, mri, "Exposure needs to be greater than zero"
+                )
+            )
 
     def _get_detector_exposure_times(
         self, detectors: ADetectorTable
     ) -> Tuple[float, float]:
         assert detectors, "No detector table found"
-        diff_detector_found = False
-        tomo_detector_found = False
-        diff_detector_exposure = 0.0
-        tomo_detector_exposure = 0.0
+        diffraction_detector_found = False
+        imaging_detector_found = False
+        diffraction_detector_exposure = 0.0
+        imaging_detector_exposure = 0.0
         for enable, name, mri, exposure, frames in detectors.rows():
-            if name == self.diff_detector and enable:
-                assert frames == 1, "%s (mri %s) can only do 1 frame per step" % (
-                    name,
-                    mri,
-                )
-                diff_detector_exposure = exposure
-                diff_detector_found = True
-            elif name == self.tomo_detector and enable:
-                assert frames == 1, "%s (mri %s) can only do 1 frame per step" % (
-                    name,
-                    mri,
-                )
-                tomo_detector_exposure = exposure
-                tomo_detector_found = True
-            if diff_detector_found and tomo_detector_found:
+            if name == self.diffraction_detector:
+                self._check_detector_parameters(name, mri, frames, enable, exposure)
+                diffraction_detector_exposure = exposure
+                diffraction_detector_found = True
+            elif name == self.imaging_detector:
+                self._check_detector_parameters(name, mri, frames, enable, exposure)
+                imaging_detector_exposure = exposure
+                imaging_detector_found = True
+            if diffraction_detector_found and imaging_detector_found:
                 break
-        if not diff_detector_found:
+        if not diffraction_detector_found:
             raise ValueError("Diffraction detector not found in table")
-        elif not tomo_detector_found:
+        elif not imaging_detector_found:
             raise ValueError("Tomography detector not found in table")
 
-        return diff_detector_exposure, tomo_detector_exposure
+        return diffraction_detector_exposure, imaging_detector_exposure
 
     def _get_time_at_positions(
         self, part_info: scanning.hooks.APartInfo, detectors: ADetectorTable
     ) -> Tuple[float, float]:
         # Find out the exposure times of our detectors
         (
-            diff_detector_exposure,
-            tomo_detector_exposure,
+            diffraction_detector_exposure,
+            imaging_detector_exposure,
         ) = self._get_detector_exposure_times(detectors)
 
         # Increase the time at each position to the minimum turnaround if necessary
         min_turnaround = get_min_turnaround_and_interval(part_info)[0]
-        time_at_diff_position = max(min_turnaround, diff_detector_exposure)
-        time_at_tomo_position = max(min_turnaround, tomo_detector_exposure)
+        time_at_diffraction_position = max(
+            min_turnaround, diffraction_detector_exposure
+        )
+        time_at_imaging_position = max(min_turnaround, imaging_detector_exposure)
 
-        return time_at_diff_position, time_at_tomo_position
+        return time_at_diffraction_position, time_at_imaging_position
 
-    def _calculate_cycle_duration(self, time_at_diff_position, time_at_tomo_position):
-        return time_at_diff_position + time_at_tomo_position + 2 * self.move_time
+    def _calculate_cycle_duration(
+        self, time_at_diffraction_position: float, time_at_imaging_position: float
+    ):
+        return (
+            time_at_diffraction_position + time_at_imaging_position + 2 * self.move_time
+        )
+
+    def _check_generator_is_static(self, primary_generator: Generator) -> None:
+        assert isinstance(
+            primary_generator, StaticPointGenerator
+        ), f"Expected primary generator to be static, got {type(primary_generator)}"
 
     @add_call_types
     def on_validate(
@@ -119,13 +150,17 @@ class BeamSelectorPart(PmacChildPart):
         generator: scanning.hooks.AGenerator,
         detectors: ADetectorTable,
     ) -> scanning.hooks.UParameterTweakInfos:
+        # Check the primary generator is static
+        self._check_generator_is_static(generator.generators[0])
+
         # Calculate the time that should be spent at each position
-        time_at_diff_position, time_at_tomo_position = self._get_time_at_positions(
-            part_info, detectors
-        )
+        (
+            time_at_diffraction_position,
+            time_at_imaging_position,
+        ) = self._get_time_at_positions(part_info, detectors)
         # Now calculate how long one cycle should take
         cycle_duration = self._calculate_cycle_duration(
-            time_at_diff_position, time_at_tomo_position
+            time_at_diffraction_position, time_at_imaging_position
         )
         # Return the generator with our cycle duration
         serialized = generator.to_dict()
@@ -144,37 +179,49 @@ class BeamSelectorPart(PmacChildPart):
         detectors: ADetectorTable,
         axesToMove: scanning.hooks.AAxesToMove,
     ) -> None:
+        # Check the provided generator is formatted as expected
+        static_axis = generator.generators[0]
+        self._check_generator_is_static(static_axis)
 
         # Calculate the time that should be spent at each position
-        time_at_diff_position, time_at_tomo_position = self._get_time_at_positions(
-            part_info, detectors
+        (
+            time_at_diffraction_position,
+            time_at_imaging_position,
+        ) = self._get_time_at_positions(part_info, detectors)
+
+        # Check that the duration matches the expected output from Validate
+        expected_duration = self._calculate_cycle_duration(
+            time_at_diffraction_position, time_at_imaging_position
         )
+        assert (
+            generator.duration == expected_duration
+        ), f"Expected a duration of {expected_duration}, got {generator.duration}"
 
         # Build our mutator
         mutator = AlternatingDelayAfterMutator(
-            time_at_diff_position, time_at_tomo_position
+            time_at_diffraction_position, time_at_imaging_position
         )
 
-        # Double the number of points to get rotations (2 points per cycle)
-        static_axis = generator.generators[0]
-        assert isinstance(
-            static_axis, StaticPointGenerator
-        ), "Static Point Generator not configured correctly"
+        # Double the number of points so we have 2 points per complete cycle
         static_axis = StaticPointGenerator(size=static_axis.size * 2)
         steps_to_do *= 2
 
-        # Create a linear scan axis (proper rotation)
+        # Create a linear scan axis to handle motion between the positions
         selector_axis = LineGenerator(
             self.selector_axis,
             "deg",
-            self.tomo_angle,
-            self.diff_angle,
+            self.imaging_angle,
+            self.diffraction_angle,
             1,
             alternate=True,
         )
         axesToMove = [self.selector_axis]
 
-        # Build the compound generator for the PMAC for handling the motion
+        # Build the compound generator for the PMAC for handling the motion.
+        # We build this in configure because the duration may cause some issues
+        # when trying to validate the detectors taking part due to comparing it
+        # to their exposure times - which is not relevant for this scan as the
+        # exposures will be during the delay_after phase at each position.
         new_generator = CompoundGenerator(
             [static_axis, selector_axis],
             [],
