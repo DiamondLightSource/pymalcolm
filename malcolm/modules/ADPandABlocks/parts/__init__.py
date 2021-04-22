eae87f833731f87ac300eebf289626f36ba7eb6d
Ben Bradnick <ben.bradnick@diamond.ac.uk>
2021-04-22 10:35:15 +0100

    PandAAlternatingDivPart: replace with more generic MultiTriggerPart which can accept combinations of valid trigger multipliers

--- malcolm/modules/ADPandABlocks/parts/__init__.py
+++ malcolm/modules/ADPandABlocks/parts/__init__.py
@@ -2,7 +2,6 @@
 from malcolm.core import submodule_all
 
 from .kinematicssavupart import KinematicsSavuPart
-from .pandaalternatingdivpart import PandAAlternatingDivPart
 from .pandapulsetriggerpart import AInitialVisibility, PandAPulseTriggerPart
 from .pandaseqtriggerpart import AMri, APartName, PandASeqTriggerPart
 
