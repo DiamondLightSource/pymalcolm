868efbeecb92c586b54a845ef36457c8534e6f36
Benjamin Bradnick <ben.bradnick@diamond.ac.uk>
2021-06-30 09:57:39 +0100

    Add unrolling block to allow enabling/disabling part based on visibility in design

--- malcolm/modules/scanning/blocks/__init__.py
+++ malcolm/modules/scanning/blocks/__init__.py
@@ -4,5 +4,6 @@ shutter_block = make_block_creator(__file__, "shutter_block.yaml")
 scan_runner_block = make_block_creator(__file__, "scan_runner_block.yaml")
 attribute_block = make_block_creator(__file__, "attribute_block.yaml")
 directory_monitor_block = make_block_creator(__file__, "directory_monitor_block.yaml")
+unrolling_block = make_block_creator(__file__, "unrolling_block.yaml")
 
 __all__ = check_yaml_names(globals())
--- malcolm/modules/scanning/parts/unrollingpart.py
+++ malcolm/modules/scanning/parts/unrollingpart.py
@@ -1,17 +1,28 @@
 from annotypes import add_call_types
 from scanpointgenerator import CompoundGenerator, SquashingExcluder
 
-from malcolm.core import Part, PartRegistrar
+from malcolm.core import AMri, APartName, PartRegistrar
+from malcolm.modules.builtin.parts import ChildPart
+from malcolm.modules.builtin.parts.childpart import AStateful, UInitialVisibility
 
 from ..hooks import AAxesToMove, AGenerator, UParameterTweakInfos, ValidateHook
 from ..infos import ParameterTweakInfo
 
 
-class UnrollingPart(Part):
+class UnrollingPart(ChildPart):
     """Unroll the dimensions described by axesToMove into one long line by
     inserting a SquashingExcluder into the generator. This is used for instance
     in Odin to unroll a snake scan into a long line so the VDS is performant
     """
+
+    def __init__(
+        self,
+        name: APartName,
+        mri: AMri,
+        initial_visibility: UInitialVisibility = None,
+        stateful: AStateful = False,
+    ) -> None:
+        super().__init__(name, mri, initial_visibility, stateful)
 
     # This will be serialized, so maintain camelCase for axesToMove
     # noinspection PyPep8Naming
@@ -19,6 +30,9 @@ class UnrollingPart(Part):
     def on_validate(
         self, generator: AGenerator, axesToMove: AAxesToMove
     ) -> UParameterTweakInfos:
+        if not self.visible:
+            return None
+
         if len(axesToMove) in (0, 1):
             # We can't have multiple dimensions here, so this must be ok
             return None
