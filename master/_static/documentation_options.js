7d807cd99383f1e7c34afc6565fb0adec0c07186
BenBradnick <BenBradnick@users.noreply.github.com>
2021-12-16 09:57:26 +0000

    deploy: 2d501e9204dc2b40445dacada5696096793420a0

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.0+5.g166dc803',
+    VERSION: '5.0+15.g2d501e92',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
