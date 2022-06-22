59de068866633f624298d6113d6e71c0ce256097
EdWarrick <edmund.warrick@diamond.ac.uk>
2022-06-22 13:11:07 +0100

    Fixed unit test for EigerDriverPart

--- tests/test_modules/test_ADEiger/test_eigerdriverpart.py
+++ tests/test_modules/test_ADEiger/test_eigerdriverpart.py
@@ -2,8 +2,9 @@ from mock import call
 from scanpointgenerator import CompoundGenerator, LineGenerator
 
 from malcolm.core import Context, Process
-from malcolm.modules.ADEiger.blocks import eiger_driver_block
-from malcolm.modules.ADEiger.parts import EigerDriverPart
+from malcolm.modules.ADEiger.blocks import eiger_driver_block, eiger_runnable_block
+from malcolm.modules.ADEiger.parts import EigerDriverPart, EigerOdinWriterPart
+from malcolm.modules.ADOdin.blocks import odin_writer_block
 from malcolm.testutil import ChildTestCase
 
 
@@ -18,7 +19,13 @@ class TestEigerDetectorDriverPart(ChildTestCase):
             prefix="prefix",
             fan_prefix="FAN",
         )
-        self.o = EigerDriverPart(name="m", mri="mri")
+        self.writer_child = self.create_child_block(
+            odin_writer_block,
+            self.process,
+            mri="writer_mri",
+            prefix="writer_prefix"
+        )
+        self.o = EigerDriverPart(name="m", mri="mri", writer_mri="writer_mri")
         self.context.set_notify_dispatch_request(self.o.notify_dispatch_request)
         self.process.start()
 
@@ -32,6 +39,9 @@ class TestEigerDetectorDriverPart(ChildTestCase):
         generator.prepare()
         completed_steps = 0
         steps_to_do = 2000 * 3000
+        # Eiger driver is coupled to writer as must be started after file paths writer on writer, 
+        # so set running here to pass the check in arm_detector 
+        self.set_attributes(self.writer_child, running=True)
         # We wait to be armed, so set this here
         self.set_attributes(self.child, acquiring=True, fanStateReady=1)
         self.o.on_configure(
